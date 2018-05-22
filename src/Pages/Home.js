a080a9cd22d0f92fce48e3da19fe3072ff4a58fb
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-22 01:40:03 -0300

    Cria component Home

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { spotifyAuthorizeUrl } from '../constants';
+
+const Home = () => ((
+  <React.Fragment>
+    <p>Login in your Spotify account to see the featured playlists we have prepared for you.</p>
+    <a href={spotifyAuthorizeUrl}>Authorize</a>
+  </React.Fragment>
+));
+
+export default Home;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const spotifyAuthorizeUrl = 'https://accounts.spotify.com/authorize?client_id=5bed112155354bd9a18876c5d8dabcb9&response_type=token&redirect_uri=http://localhost:8080/authorize';
