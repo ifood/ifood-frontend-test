2de05c7bc01e22d69df6ea2060bc3a70df3a035d
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-22 01:40:16 -0300

    Cria component List

---
+++ src/Pages/List.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Filters from '../components/Filters/Filters';
+import PlaylistCard from '../components/PlaylistCard/PlaylistCard';
+
+import { getPlaylists } from '../services/spotifyServices';
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playlists: [],
+    };
+  }
+
+  componentDidMount() {
+    getPlaylists()
+      .then((response) => {
+        const playlists = response.data.playlists.items;
+        this.setState({
+          playlists,
+        });
+      });
+  }
+
+  render() {
+    const { playlists } = this.state;
+    return (
+      <React.Fragment>
+        <Filters />
+        {
+          playlists.map(playlist => ((
+            <PlaylistCard
+              href={playlist.external_urls.spotify}
+              image={playlist.images[0].url}
+              name={playlist.name}
+              key={playlist.id}
+            />
+          )))
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+export default List;
