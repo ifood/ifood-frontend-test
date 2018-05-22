1998c4d19ec7274c262008bc38f2fd78d0684b24
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-21 23:34:39 -0300

    configura setup inicial

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const { join } = require('path');
+
+const paths = {
+  styles: join(__dirname, '..', 'src', 'styles')
+};
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              includePaths: ['node_modules', paths.styles]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    htmlPlugin
+  ]
+};
