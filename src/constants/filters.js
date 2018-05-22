58a1af3c76306d277f18643ac75e1694b8df242c
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-22 03:20:11 -0300

    move constantes e adiciona novas para filters

---
+++ src/constants/filters.js
@@ -0,0 +1,9 @@
+export const locale = 'locale';
+export const country = 'country';
+export const timestamp = 'timestamp';
+export const limit = 'limit';
+export const offset = 'offset';
+
+export const filtersFields = [
+  locale, country, timestamp, limit, offset,
+];
