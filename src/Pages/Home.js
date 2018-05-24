6f1ba5629dc9e56bb91db0ea5e121fb790ff34e6
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-24 07:25:25 -0300

    estiliza home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import { SPOTIFY_AUTHORIZE_URL } from '../constants/spotify';
 
+
 const Home = () => ((
-  <React.Fragment>
-    <p>Login in your Spotify account to see the featured playlists we have prepared for you.</p>
-    <a href={SPOTIFY_AUTHORIZE_URL}>Authorize</a>
-  </React.Fragment>
+  <div className="container">
+    <Typography variant="headline" gutterBottom>
+      Login in your Spotify account to see the featured playlists we have prepared for you.
+    </Typography>
+
+    <a className="auth-link" href={SPOTIFY_AUTHORIZE_URL}>Authorize</a>
+  </div>
 ));
 
 export default Home;
