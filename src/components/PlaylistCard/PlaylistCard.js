1197b4bf296306e73b6b090b9250ad4763efcfa4
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-24 07:11:45 -0300

    refatora usando Card do material ui

--- src/components/PlaylistCard/PlaylistCard.js
+++ src/components/PlaylistCard/PlaylistCard.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const PlaylistCard = ({
   href,
   name,
   image,
 }) => ((
-  <div className="card">
-    <div className="card-image">
-      <a href={href} target="_blank">
-        <figure className="image is-4by3">
-          <img src={image} alt="playlist picure" />
-        </figure>
-        <span className="card-title">{name}</span>
-      </a>
-    </div>
-  </div>
+  <Card className="playlist-card">
+    <a href={href} target="_blank">
+      <CardMedia
+        className="playlist-card__image"
+        image={image}
+      />
+      <CardContent className="playlist-card__content">
+        <Typography
+          gutterBottom
+          component="h2"
+          className="playlist-card__name"
+        >
+          {name}
+        </Typography>
+      </CardContent>
+    </a>
+  </Card>
 ));
 
 PlaylistCard.propTypes = {
