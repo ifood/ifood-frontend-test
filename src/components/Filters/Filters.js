9352f10d528780fbebb9b1246b11d8a39f3e9850
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-05-24 07:10:36 -0300

    usa input do material

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
 
 import { getFilters } from '../../services/filterServices';
 
@@ -34,14 +35,19 @@ class Filters extends Component {
     const { filters } = this.state;
     const { filterByName } = this.props;
     return (
-      <React.Fragment>
-        <input type="text" placeholder="Search by name" onChange={filterByName} />
+      <div className="filters-container">
+        <TextField
+          id="search"
+          label="Search by name"
+          onChange={filterByName}
+          margin="normal"
+        />
         {
           filters.map(field => ((
             <RenderField {...field} key={field.id} />
           )))
         }
-      </React.Fragment>
+      </div>
     );
   }
 }
