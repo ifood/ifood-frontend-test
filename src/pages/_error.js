d8d230542fce352ceaaa0c0c3cc9a284c3f31479
Ricardo Teixeira <ricardo_teixeira@hotmail.com.br>
2019-12-11 00:13:42 -0300

    Initial setup with CRA and Next + linting

---
+++ src/pages/_error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Error({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server`
+        : 'An error occurred on client'}
+    </p>
+  )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const error = res ? res.statusCode : err
+  const statusCode = error ? err.statusCode : 404
+  return { statusCode }
+}
+
+export default Error
