38fdeacb5527c1cc83b796c0d2f3ebbf42f9ba70
Ricardo Teixeira <ricardo_teixeira@hotmail.com.br>
2019-12-19 11:41:44 -0300

    adding heroku deploy

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ module.exports = withCSS({
       '/callback': { page: ROUTES.CALLBACK }
     }
   },
-  assetPrefix: !debug ? '/ifood-frontend-test/' : '',
+  // assetPrefix: !debug ? '/ifood-frontend-test/' : '',
   env: {
     CLIENT_ID: 'a62f3983edba4acdbf505b5f49c7d07e',
     REDIRECT_URI: debug
       ? 'http://localhost:3000/callback'
-      : 'https://ricardo-teixeira.github.io/ifood-frontend-test/callback'
+      : 'https://spotifood-test.herokuapp.com/callback'
   }
 })
