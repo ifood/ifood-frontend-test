604fb4399cd5fa17ec6156756938284c3e7fc495
degrecci <henrique.degrecci@upx.com>
2020-02-02 20:17:16 -0300

    Insert drawer for filters

--- src/pages/Playlists.js
+++ src/pages/Playlists.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Drawer from '@material-ui/core/Drawer';
 import Filters from '../components/Filters/Filters';
+
 
 class Playlists extends Component {
   render() {
     return (
-      <Filters />
+      <Drawer variant="permanent">
+        <Filters />
+      </Drawer>
     );
   }
 }
