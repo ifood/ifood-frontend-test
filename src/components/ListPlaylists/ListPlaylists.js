075d90d3596439b605dbc70d3a96b7061cfe8ea6
degrecci <henrique.degrecci@upx.com>
2020-02-03 22:50:39 -0300

    Add local filter for playlists

--- src/components/ListPlaylists/ListPlaylists.js
+++ src/components/ListPlaylists/ListPlaylists.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  GridList, GridListTile, IconButton, GridListTileBar, CircularProgress,
+  GridList, GridListTile, IconButton, GridListTileBar, CircularProgress, TextField,
 } from '@material-ui/core';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import { getFormValues } from 'redux-form';
@@ -13,13 +13,14 @@ class ListPlaylists extends Component {
   state = {
     isLoading: true,
     playlists: [],
+    name: '',
   }
 
   componentDidMount() {
     this.listFeaturedPlaylists();
   }
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const { formValues } = this.props;
 
     if (this.state !== prevState) {
@@ -29,36 +30,56 @@ class ListPlaylists extends Component {
     this.listFeaturedPlaylists(formValues);
   }
 
+  _handleNameChange = (event) => {
+    const { value } = event.target;
+    this.setState({ name: value });
+  }
+
   async listFeaturedPlaylists(params = {}) {
     const playlists = await getFeaturedPlaylists(params);
     this.setState({ isLoading: false, playlists });
   }
 
   render() {
-    const { isLoading, playlists } = this.state;
+    const { isLoading, playlists, name } = this.state;
+    const _filterPlaylistsByName = (playlist) => (
+      playlist.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+    );
+
     return (
       <>
         {isLoading && <CircularProgress />}
-        <GridList cellHeight={300}>
-          {!isLoading && playlists.map((playlist, index) => (
-            <GridListTile key={index}>
-              <img src={playlist.images[0].url} alt={playlist.name} />
-              <GridListTileBar
-                title={playlist.name}
-                subtitle={(
-                  <span>
-                    {playlist.description}
-                  </span>
-)}
-                actionIcon={(
-                  <IconButton aria-label={`Link para a playlist ${playlist.name}`}>
-                    <OpenInNewIcon />
-                  </IconButton>
-              )}
-              />
-            </GridListTile>
-          ))}
-        </GridList>
+        {!isLoading && (
+          <>
+            <TextField
+              margin="normal"
+              label="Buscar playlists"
+              variant="outlined"
+              onChange={this._handleNameChange}
+              fullWidth
+            />
+            <GridList cellHeight={300}>
+              {playlists.filter(_filterPlaylistsByName).map((playlist, index) => (
+                <GridListTile key={index}>
+                  <img src={playlist.images[0].url} alt={playlist.name} />
+                  <GridListTileBar
+                    title={playlist.name}
+                    subtitle={(
+                      <span>
+                        {playlist.description}
+                      </span>
+                    )}
+                    actionIcon={(
+                      <IconButton aria-label={`Link para a playlist ${playlist.name}`}>
+                        <OpenInNewIcon />
+                      </IconButton>
+                    )}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </>
+        )}
       </>
     );
   }
