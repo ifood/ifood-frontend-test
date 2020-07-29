76208a9af5bdeeddd3c2e51ce5ea0422d38dd57e
Cleison Sousa <cleison.sousa@wooza.com.br>
2020-07-29 20:47:21 -0300

    style: add component card-featured for featured-playlist

--- src/components/card-featured/index.js
+++ src/components/card-featured/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function CardFeatured() {
+import * as S from './styles'
+
+import Background from './background'
+
+export default function CardFeatured ({ playlists }) {
   return (
-    <h2>Card Featured</h2>
+    <S.Container>
+      <S.Content>
+        <S.Title>Featured Playlists</S.Title>
+        <S.WrapperCard>
+          {playlists.items && playlists.items.map(item => (
+            <S.Card key={item.id}>
+              <S.LinkSpotify href={item.external_urls.spotify} target='_blank'>
+                <Background
+                  bg={item.images[0].url}
+                  title={item.name}
+                />
+              </S.LinkSpotify>
+              <S.Description>{item.description}</S.Description>
+            </S.Card>
+          ))}
+        </S.WrapperCard>
+      </S.Content>
+    </S.Container>
   )
+}
+
+CardFeatured.propTypes = {
+  playlists: PropTypes.shape({
+    items: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      images: PropTypes.arrayOf(PropTypes.shape({
+        url: PropTypes.string
+      }))
+    }))
+  }).isRequired
 }
---
+++ src/components/card-featured/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.section``
+
+export const Content = styled.div`
+  margin-top: 40px;
+`
+
+export const WrapperCard = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+`
+
+export const Card = styled.div`
+  margin-right: 15px;
+  width: 200px;
+`
+
+export const Title = styled.h1``
+
+export const LinkSpotify = styled.a``
+
+export const Description = styled.p`
+  font-size: 0.75rem;
+  text-align: justify;
+  margin: 5px 0 10px;
+`
