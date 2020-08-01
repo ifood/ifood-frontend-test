24903b884c42bfeb66f0a9025b736bc5fd75e264
Cleison Sousa <cleison.sousa@wooza.com.br>
2020-08-01 00:41:15 -0300

    feat: add component header, layout and stories for test UI

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import * as S from './styles'
+
+export default function Header ({ children }) {
+  return (
+    <S.Container>
+      <S.Content>
+        {children}
+      </S.Content>
+    </S.Container>
+  )
+}
+
+Header.propTypes = {
+  children: PropTypes.element.isRequired
+}
