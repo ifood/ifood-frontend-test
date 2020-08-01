d60783542b67158ce1a9d1e93f85b1deeec69c0c
Cleison Sousa <cleison.sousa@wooza.com.br>
2020-08-01 18:36:00 -0300

    refactor: pattern eslint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
-import GlobalStyle from './styles/global';
-import Theme from './styles/theme';
+import GlobalStyle from './styles/global'
+import Theme from './styles/theme'
+import Svgs from './components/svgs'
 
-import Router from './pages/router';
+import Router from './pages/router'
 
-function App() {
+function App () {
   return (
-    <ThemeProvider theme={ Theme }>
+    <ThemeProvider theme={Theme}>
       <GlobalStyle />
+      <Svgs />
       <Router />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
