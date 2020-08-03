2a111f84c70f2b019851e76c0c07b17209229e40
Cleison Sousa <cleison.sousa@wooza.com.br>
2020-08-02 23:50:11 -0300

    feat: add logic for handler filters to get playlists

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,45 +1,161 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { MdFilterList } from 'react-icons/md'
 
 import * as S from './styles'
 
-export default function Filters ({ filters, handleOnSubmitFilters }) {
+import Search from './../search'
+import Input from './../input'
+import Button from './../button'
+
+export default function Filters ({ filters, handlerSubmitFilters, handlerLanguage, filterPLaylists }) {
+  const [limit, setLimit] = useState(5)
+  const [offset, setOffset] = useState(0)
+  const [timestamp, setTimesatamp] = useState('2014-10-23T09:00:00')
   const [opened, setOpened] = useState(false)
-  const [searchies, setSearchies] = useState({})
+  const [searchies, setSearchies] = useState({ country: '', locale: '' })
+  const [propsInput, setPropsInput] = useState({})
+
+  useEffect(() => {
+    const props = buildInputProps(filters)
+
+    setPropsInput(props)
+  }, [filters])
+
+  function buildInputProps (filters) {
+    const props = {}
+
+    filters.forEach((filter) => {
+      if (filter.validation) {
+        if (filter.id === 'timestamp') {
+          props.timestamp = {
+            type: getTypeInput(filter.validation.primitiveType),
+            defaultValue: filter.validation.pattern,
+            // pattern: filter.validation.pattern,
+            onChange: (e) => setTimesatamp(e.target.value)
+          }
+        }
+
+        if (filter.id === 'limit') {
+          props.limit = {
+            type: getTypeInput(filter.validation.primitiveType),
+            min: filter.validation.min,
+            max: filter.validation.max,
+            defaultValue: limit,
+            onChange: (e) => setLimit(e.target.value)
+          }
+        }
+
+        if (filter.id === 'offset') {
+          props.offset = {
+            type: getTypeInput(filter.validation.primitiveType),
+            defaultValue: offset,
+            onChange: (e) => setOffset(e.target.value)
+          }
+        }
+
+        props[`${filter.id}`].id = filter.id
+      }
+    })
+    return props
+  }
+
+  function getTypeInput (type) {
+    const types = {
+      STRING: () => 'text',
+      INTEGER: () => 'number'
+    }
+
+    return types[`${type}`]() || 'text'
+  }
 
   function selectSearchies (search) {
     const searchCopy = Object.assign({}, searchies)
-    searchCopy[`${search.id}`] = search.value
 
-    setSearchies(searchCopy)
+    if (search.value.name !== searchies[`${search.id}`].name) {
+      searchCopy[`${search.id}`] = search.value
+
+      if (search.id === 'locale') handlerLanguage(search.value.value)
+
+      if (search.id === 'country') {
+        const { value } = handlerLocaleByCountry(search.value.value)
+
+        handlerLanguage(value)
+      }
+
+      setSearchies(searchCopy)
+    } else {
+      searchCopy[`${search.id}`] = ''
+
+      setSearchies(searchCopy)
+    }
+  }
+
+  function handlerLocaleByCountry (value) {
+    const abreviationCountry = value.slice(-2)
+
+    const { values } = filters.find(filter => filter.id === 'locale')
+
+    const abreviationLocale = values.find(i => i.value.endsWith(`${abreviationCountry}`))
+    return abreviationLocale || { value: 'en_US', name: 'en_US' }
+  }
+
+  async function loadByFilters (e) {
+    e.preventDefault()
+
+    const newSearchies = {
+      country: searchies.country.value,
+      locale: searchies.locale.value,
+      offset,
+      limit,
+      timestamp
+    }
+
+    setOpened(!opened)
+    handlerSubmitFilters(newSearchies)
   }
 
   return (
     <S.Container>
       <S.Content>
-        <S.WrapperIcon onClick={() => setOpened(!opened)}>
+        <Button
+          type='button'
+          template='filter'
+          handleClick={() => setOpened(!opened)}>
           <S.Title>Filtros</S.Title>
-          <MdFilterList size={24} />
-        </S.WrapperIcon>
+          <S.IconFilter />
+        </Button>
+        <Search handleChange={filterPLaylists} />
+        <S.IconCountry name={`icon-${searchies.country && searchies.country.value.toLowerCase()}`} alt='Country' />
         <S.WrapperFilters opened={opened}>
-          {filters.map((filter) => (
-            <S.Filtro
-              key={filter.id}>
-              <S.Title>{filter.name}</S.Title>
-              <S.Values>
-                {filter.values && filter.values.map((value, index) => (
-                  <S.Value
-                    selected={searchies[`${filter.id}`] && searchies[`${filter.id}`].name === value.name}
-                    key={index}
-                    onClick={() => selectSearchies({ value, id: filter.id })}>
-                    {value.name}
-                  </S.Value>
-                ))}
-              </S.Values>
-            </S.Filtro>
-          ))}
-          <S.Button onClick={async () => handleOnSubmitFilters(searchies)}>Aplicar filtros</S.Button>
+          <S.Form onSubmit={(e) => loadByFilters(e)} >
+            {filters.map((filter) => (
+              <S.Filtro
+                key={filter.id}>
+                <S.SubTitle htmlFor={filter.id}>{filter.name}</S.SubTitle>
+                {filter.values &&
+                  <S.Values>
+                    {filter.values.map((value, index) => (
+                      <S.Value
+                        id={filter.id}
+                        selected={searchies[`${filter.id}`] && searchies[`${filter.id}`].name === value.name}
+                        key={index}
+                        onClick={() => selectSearchies({ value, id: filter.id })}>
+                        {value.name}
+                      </S.Value>
+                    ))}
+                  </S.Values>
+                }
+                {filter.validation &&
+                  <S.WrapperValidation>
+                    <Input props={propsInput[`${filter.id}`]} />
+                  </S.WrapperValidation>
+                }
+              </S.Filtro>
+            ))}
+            <Button type='submit'>
+              Aplicar filtros
+            </Button>
+          </S.Form>
         </S.WrapperFilters>
       </S.Content>
     </S.Container>
@@ -48,5 +164,7 @@ export default function Filters ({ filters, handleOnSubmitFilters }) {
 
 Filters.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.object).isRequired,
-  handleOnSubmitFilters: PropTypes.func.isRequired
+  filterPLaylists: PropTypes.func.isRequired,
+  handlerLanguage: PropTypes.func.isRequired,
+  handlerSubmitFilters: PropTypes.func.isRequired
 }
--- src/components/filters/styles.js
+++ src/components/filters/styles.js
@@ -1,56 +1,115 @@
 import styled from 'styled-components'
+import { MdFilterList } from 'react-icons/md'
+
+import media from '../../styles/media-queries'
+import Icon from './../icon'
 
 export const Container = styled.div`
   align-items: center;
   display: flex;
+  flex: 1;
 `
 
-export const Content = styled.div` `
+export const Content = styled.div`
+  align-items: center;
+  color: ${props => props.theme.palette.white};
+  display: flex;
+  flex : 1;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 1140px;
+  padding: 0 15px;
+`
 
 export const WrapperIcon = styled.div`
   align-items: center;
-  border: 1px solid ${props => props.theme.palette.gray};
-  border-radius: 20px;
+  background-color: ${props => props.theme.palette.white};
   cursor: pointer;
   display: flex;
-  padding: 0px 20px;
+  justify-content: center;
+
+  ${media.mobile`
+    height: 28px;
+    width: 28px;
+    border-radius: 100%;
+  `}
+  ${media.desktop`
+    border-radius: 20px;
+    margin-right: 25px;
+    padding: 0px 20px;
+  `}
 `
 
-export const Title = styled.span``
+export const IconCountry = styled(Icon)`
+  height: 28px;
+  width: 28px;
+`
+
+export const Title = styled.span`
+  ${media.mobile`
+    display: none;
+  `}
+`
+
+export const IconFilter = styled(MdFilterList)`
+  fill: ${props => props.theme.palette.black};
+  font-size: 22px;
+`
+
+export const SubTitle = styled.label`
+  display: block;
+  margin-bottom: 5px;
+`
 
 export const WrapperFilters = styled.div`
-  background-color: ${props => props.theme.palette.white};
-  border-radius: 20px;
-  padding: 0px 20px;
+  background-image: linear-gradient(130deg, #c074b2, #8ab5e8);
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
   position: absolute;
-  width: 500px;
   overflow: hidden;
   transition: height 0.7s, border 0.6s;
-  top: 40px;
   z-index: 100;
+
+  ${media.mobile`
+    background-image: linear-gradient(80deg, #c074b2, #8ab5e8);
+    left: 0px;
+    top: 47px;
+    width: 100%;
+  `}
+  ${media.desktop`
+    background-image: linear-gradient(130deg, #c074b2, #8ab5e8);
+    top: 67px;
+    width: 380px;
+  `}
 
   ${props => props.opened ? `
       height: 500px;
-      border: 1px solid ${props.theme.palette.gray};
     ` : `
-      border: 0px solid ${props.theme.palette.gray};
       height: 0;
     `
   }
 `
 
+export const Form = styled.form`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 20px;
+  height: 100%;
+`
+
 export const Filtro = styled.div`
-  margin-top: 15px;
+  margin-bottom: 20px;
 `
 
 export const Values = styled.ul`
+  color: ${props => props.theme.palette.black};
   display: flex;
   flex-wrap: wrap;
-  margin-top: 5px;
 `
 
+export const WrapperValidation = styled.div``
+
 export const Value = styled.li`
-  border: 1px solid ${props => props.theme.palette.gray};
+  background-color: ${props => props.theme.palette.white};
   border-radius: 20px;
   cursor: pointer;
   padding: 3px 15px;
@@ -58,9 +117,6 @@ export const Value = styled.li`
 
   ${props => props.selected && `
       background-color: ${props.theme.palette.green};
-      color: ${props.theme.palette.white};
     `
   }
 `
-
-export const Button = styled.button``
