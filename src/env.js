235c6141e792ed405731a03ecf364b6e73264474
Cleison Sousa <cleison.sousa@wooza.com.br>
2020-08-04 07:25:53 -0300

    chore: add environment variables

---
+++ src/env.js
@@ -0,0 +1,10 @@
+const env = {
+  CLIENT_ID: '*<Put your client_id here>*',
+  CLIENT_SECRET: '*<Put your client_secret here>*',
+  REDIRECT_URI: '*<Put your redirect_url here>*',
+  GRANT_TYPE: 'client_credentials',
+  SCOPE: 'playlist-read-private',
+  RESPONSE_TYPE: 'code'
+}
+
+export default env
