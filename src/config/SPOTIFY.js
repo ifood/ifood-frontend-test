8aa7a35c41dc8c1092088928f9d8fa84a0ff5e16
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-10 15:06:59 -0300

    Criação de variaveis de ambiente

--- src/config/SPOTIFY.js
+++ src/config/SPOTIFY.js
@@ -1,4 +1,4 @@
-export const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize';
-export const CLIENT_ID = '3579712af8ae42d1959b4a1df703730c';
-export const REDIRECT_URI = 'http://localhost:3000';
-export const RESPONSE_TYPE = 'token';
+export const AUTHORIZE_ENDPOINT = process.env.REACT_APP_AUTHORIZE_ENDPOINT;
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+export const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE;
