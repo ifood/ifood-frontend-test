84f92de61fd0e97b7413f8a335a6d7e22907b4e2
douglas <douglassantoselias@gmail.com>
2020-08-10 23:05:54 -0300

    Add state reducer for filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,93 @@
-import { Select, DatePicker, InputNumber } from 'antd'
+import { useState, useReducer, useEffect } from 'react'
+import { Select, DatePicker, InputNumber, List } from 'antd'
 
-const { Option } = Select
-
-function handleChange(value: string) {
-  console.log(`selected ${value}`)
-}
-
-function onChange(date, dateString) {
-  console.log(date, dateString)
-}
+import { getPlaylistFilters, FilterTransformed } from '../data/playlistFilter'
 
 const Index = () => {
+  const [filters, setFilters] = useState<FilterTransformed>()
+  const [state, dispatch] = useReducer(reducer, {})
+
+  useEffect(() => {
+    getPlaylistFilters().then(setFilters)
+  }, [])
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <label>Locale</label>
+      <label>Localidade</label>
       <Select
-        defaultValue="lucy"
         style={{ width: 120 }}
-        onChange={handleChange}
-      >
-        <Option value="jack">Jack</Option>
-        <Option value="lucy">Lucy</Option>
-        <Option value="Yiminghe">yiminghe</Option>
-      </Select>
+        onChange={handleChange(dispatch)}
+        value={state.locale}
+        options={filters?.locale.values}
+      />
 
-      <label>Country</label>
+      <label>País</label>
       <Select
-        defaultValue="lucy"
         style={{ width: 120 }}
-        onChange={handleChange}
-      >
-        <Option value="jack">Jack</Option>
-        <Option value="lucy">Lucy</Option>
-        <Option value="Yiminghe">yiminghe</Option>
-      </Select>
+        onChange={handleChangeCountry(dispatch)}
+        value={state.country}
+        options={filters?.country.values}
+      />
 
       <label>Date</label>
-      <DatePicker style={{ width: 120 }} onChange={onChange} />
+      <DatePicker
+        style={{ width: 120 }}
+        onChange={onChange(dispatch)}
+        // value={state.timestamp}
+      />
 
       <label>Limit</label>
-      <InputNumber style={{ width: 120 }} min={1} max={50} />
+      <InputNumber
+        style={{ width: 120 }}
+        value={state.limit}
+        min={filters?.limit.min}
+        max={filters?.limit.max}
+      />
 
       <label>Quantity</label>
-      <InputNumber style={{ width: 120 }} />
+      <InputNumber style={{ width: 120 }} min={0} value={state.offset} />
     </div>
   )
 }
 
 export default Index
+
+function handleChange(dispatch: Dispatch) {
+  return (value: string) => {
+    dispatch({ type: 'locale', payload: value })
+  }
+}
+
+function handleChangeCountry(dispatch: Dispatch) {
+  return (value: string) => {
+    dispatch({ type: 'country', payload: value })
+  }
+}
+
+function onChange(dispatch: Dispatch) {
+  return (date: any, dateString: string) => {
+    dispatch({ type: 'country', payload: dateString })
+    console.log(date, dateString)
+  }
+}
+
+function reducer(state: State, action: Action): State {
+  const clonedState = { ...state }
+  clonedState[action.type] = action.payload
+  return clonedState
+}
+
+type Dispatch = (action: Action) => void
+
+type Action = {
+  type: string
+  payload: any
+}
+
+type State = {
+  locale?: string
+  country?: string
+  timestamp?: string
+  limit?: number
+  offset?: number
+}
