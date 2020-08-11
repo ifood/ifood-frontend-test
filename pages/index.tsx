d6c7bce59e862eac046d9b387e6fc1a9a0390c47
douglas <douglassantoselias@gmail.com>
2020-08-10 21:34:12 -0300

    Add sample filter components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,49 @@
-import { InputNumber } from 'antd'
+import { Select, DatePicker, InputNumber } from 'antd'
+
+const { Option } = Select
+
+function handleChange(value: string) {
+  console.log(`selected ${value}`)
+}
+
+function onChange(date, dateString) {
+  console.log(date, dateString)
+}
 
 const Index = () => {
   return (
-    <>
-      <InputNumber />
-    </>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <label>Locale</label>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="Yiminghe">yiminghe</Option>
+      </Select>
+
+      <label>Country</label>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="Yiminghe">yiminghe</Option>
+      </Select>
+
+      <label>Date</label>
+      <DatePicker style={{ width: 120 }} onChange={onChange} />
+
+      <label>Limit</label>
+      <InputNumber style={{ width: 120 }} min={1} max={50} />
+
+      <label>Quantity</label>
+      <InputNumber style={{ width: 120 }} />
+    </div>
   )
 }
 
