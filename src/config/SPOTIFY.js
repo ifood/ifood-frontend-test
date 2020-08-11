5bfef7ed94fc9c37ee84d5dafd727f501f7b04b0
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-11 20:07:52 -0300

    Configuração de variaveis

--- src/config/SPOTIFY.js
+++ src/config/SPOTIFY.js
@@ -1,4 +1,8 @@
-export const AUTHORIZE_ENDPOINT = process.env.REACT_APP_AUTHORIZE_ENDPOINT;
-export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-export const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE;
+export const AUTHORIZE_ENDPOINT =
+  process.env.REACT_APP_AUTHORIZE_ENDPOINT ||
+  'https://accounts.spotify.com/authorize';
+export const CLIENT_ID =
+  process.env.REACT_APP_CLIENT_ID || '3579712af8ae42d1959b4a1df703730c';
+export const REDIRECT_URI =
+  process.env.REACT_APP_REDIRECT_URI || 'http://localhost:3000';
+export const RESPONSE_TYPE = process.env.REACT_APP_RESPONSE_TYPE || 'token';
