a7e32e7172feea13e5f48ee9c41567967c5e1887
douglas <douglassantoselias@gmail.com>
2020-08-11 09:13:53 -0300

    Add list sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,69 +5,132 @@ import { getPlaylistFilters, FilterTransformed } from '../data/playlistFilter'
 
 const Index = () => {
   const [filters, setFilters] = useState<FilterTransformed>()
+  const [date, setDate] = useState()
   const [state, dispatch] = useReducer(reducer, {})
 
   useEffect(() => {
     getPlaylistFilters().then(setFilters)
   }, [])
 
+  const width = '100%'
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <label>Localidade</label>
+      <label>{filters?.locale.name}</label>
       <Select
-        style={{ width: 120 }}
-        onChange={handleChange(dispatch)}
+        style={{ width }}
+        onChange={onChangeLocale(dispatch)}
         value={state.locale}
         options={filters?.locale.values}
       />
-
-      <label>País</label>
+      <label>{filters?.country.name}</label>
       <Select
-        style={{ width: 120 }}
-        onChange={handleChangeCountry(dispatch)}
+        style={{ width }}
+        onChange={onChangeCountry(dispatch)}
         value={state.country}
         options={filters?.country.values}
       />
-
-      <label>Date</label>
+      <label>{filters?.timestamp.name}</label>
       <DatePicker
-        style={{ width: 120 }}
-        onChange={onChange(dispatch)}
-        // value={state.timestamp}
+        style={{ width }}
+        onChange={onChangeTimestamp(dispatch, setDate)}
+        value={date}
       />
-
-      <label>Limit</label>
+      <label>{filters?.limit.name}</label>
       <InputNumber
-        style={{ width: 120 }}
+        style={{ width }}
+        onChange={onChangeLimit(dispatch)}
         value={state.limit}
         min={filters?.limit.min}
         max={filters?.limit.max}
       />
+      <label>{filters?.offset.name}</label>
+      <InputNumber
+        style={{ width }}
+        onChange={onChangeOffset(dispatch)}
+        min={0}
+        value={state.offset}
+      />
 
-      <label>Quantity</label>
-      <InputNumber style={{ width: 120 }} min={0} value={state.offset} />
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={[
+          {
+            href: 'https://ant.design',
+            title: `ant design part`,
+            avatar:
+              'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+              'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+              'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+          },
+          {
+            href: 'https://ant.design',
+            title: `ant designrt`,
+            avatar:
+              'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+              'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+              'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+          },
+        ]}
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            }
+          >
+            <List.Item.Meta
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+            {item.content}
+          </List.Item>
+        )}
+      />
     </div>
   )
 }
 
 export default Index
 
-function handleChange(dispatch: Dispatch) {
+function onChangeLocale(dispatch: Dispatch) {
   return (value: string) => {
     dispatch({ type: 'locale', payload: value })
   }
 }
 
-function handleChangeCountry(dispatch: Dispatch) {
+function onChangeCountry(dispatch: Dispatch) {
   return (value: string) => {
     dispatch({ type: 'country', payload: value })
   }
 }
 
-function onChange(dispatch: Dispatch) {
+function onChangeTimestamp(dispatch: Dispatch, setState: Function) {
   return (date: any, dateString: string) => {
-    dispatch({ type: 'country', payload: dateString })
+    dispatch({ type: 'timestamp', payload: dateString })
+    setState(date)
     console.log(date, dateString)
+  }
+}
+
+function onChangeLimit(dispatch: Dispatch) {
+  return (value: number) => {
+    dispatch({ type: 'limit', payload: value })
+  }
+}
+
+function onChangeOffset(dispatch: Dispatch) {
+  return (value: number) => {
+    dispatch({ type: 'offset', payload: value })
   }
 }
 
