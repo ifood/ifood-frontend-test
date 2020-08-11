d4cbfb217aa82de386ed0288e0971f754c993b74
Alisson Matos <alissonmgsantos@gmail.com>
2020-08-11 20:31:13 -0300

    Limpeza de código

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { store } from './stores';
 import Routes from './pages/routes';
 
-import 'react-perfect-scrollbar/dist/css/styles.css';
 import './App.css';
 
 const App = () => {
