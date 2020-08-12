7d9bd105147997dfcf6ee284645b37965404ab23
douglas <douglassantoselias@gmail.com>
2020-08-12 18:11:37 -0300

    Create logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,12 @@
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+}
+
+export function Logo() {
+  return (
+    <div style={style}>
+      <img src={'logo.png'} width="50%" />
+    </div>
+  )
+}
