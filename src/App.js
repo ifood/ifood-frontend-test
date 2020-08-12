371fb9f33e72a501c749c7f5c7e828404703fde1
Lopes-7 <gui-lopes7@live.com>
2020-08-12 10:35:01 -0300

    Using all previous added features

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@
  * IMPORTS
  */
 import React from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import {Home} from './pages/home.js'; 
+import {Welcome} from './pages/welcome.js'; 
+import {store} from './store.js';
+
+
+/**
+ * STYLES
+ */
+import './App.css';
 
 
 /**
@@ -10,7 +24,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-		
+		<Provider store={store}>
+			<BrowserRouter>
+				<Switch>
+					<Redirect path="/authorized" to="/home" />
+					<Route path="/" exact component={Welcome} />
+					<Route path="/home" component={Home} />
+				</Switch>
+			</BrowserRouter>
+		</Provider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
+/**
+ * IMPORTS
+ */
 import React from 'react';
+import {transitions} from 'react-alert';
+import {Provider as AlertProvider} from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * STYLES
+ */
+import './index.css';
+const alertOptions = {
+    offset: '30px',
+    transition: transitions.SCALE,
+    timeout: 5000
+  };
 
+/**
+ * CODE
+ */
 ReactDOM.render(
   <React.StrictMode>
-        <App />
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </AlertProvider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
