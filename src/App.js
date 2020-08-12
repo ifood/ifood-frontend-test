e6c015eccf55f6ea9726ab01fb0f72864cc35db7
Lopes-7 <gui-lopes7@live.com>
2020-08-12 10:12:28 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+
+
+/**
+ * CODE
+ */
+function App() {
+  return (
+    <div className="App">
+		
+    </div>
+  );
+}
+
+
+/**
+ * EXPORTS
+ */
+export default App;
