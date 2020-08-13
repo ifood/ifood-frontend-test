3776fec296a6bccd4a8ea621de4b6eda89263443
douglas <douglassantoselias@gmail.com>
2020-08-13 09:54:07 -0300

    Change fetch to fetchURL in fetchPlaylists

--- data/playlists.ts
+++ data/playlists.ts
@@ -1,26 +1,8 @@
-const secretToken = '3b0fa2d640414630b38e2198a9060422'
+import { fetchURL } from './fetchURL'
 
 const url = 'https://api.spotify.com/v1/browse/featured-playlists?'
-export async function fetchPlaylists(filter: PlaylistFilter): Promise<any> {
-  const response = await fetch(url, { headers: { Authorization: 'Bearer ' + secretToken } })
-  const json = await response.json()
+
+export async function fetchPlaylists(accessToken: string): Promise<any> {
+  const json = await fetchURL(url, { headers: { Authorization: 'Bearer ' + accessToken } })
   return json.playlists?.items
-}
-
-export function addQueryParamsToURL(filter: PlaylistFilter) {
-  const params = new URLSearchParams()
-
-  for (const key in filter) {
-    params.append(key, filter[key])
-  }
-
-  return new URL('https://example.com?' + params.toString()).toString()
-}
-
-type PlaylistFilter = {
-  locale?: string
-  country?: string
-  timestamp?: string
-  limit?: number
-  offset?: number
 }
