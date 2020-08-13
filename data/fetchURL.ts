6d8ae456120e371060277252ed80d1743e1636eb
douglas <douglassantoselias@gmail.com>
2020-08-13 13:04:36 -0300

    Add error handling in fetch for expired token

--- data/fetchURL.ts
+++ data/fetchURL.ts
@@ -1,4 +1,8 @@
-export async function fetchURL(url: string, options?: RequestInit) {
+export async function fetchURL(url: string, options?: RequestInit): Promise<any> {
   const response = await fetch(url, options)
+
+  if (response.status === 401)
+    throw new Error('Token expirado')
+
   return response.json()
 }
