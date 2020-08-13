efe4ae60e024b7283addd0a03669f4f54f15eb11
douglas <douglassantoselias@gmail.com>
2020-08-13 19:00:10 -0300

    Add query filter function in fetch playlists

--- data/playlists.ts
+++ data/playlists.ts
@@ -6,5 +6,15 @@ const url = 'https://api.spotify.com/v1/browse/featured-playlists?'
 export async function fetchFeaturedPlaylists(accessToken: string, filters: FilterWithTimestamp): Promise<[string, Playlist[]]> {
   const queryParams = convertObjectParamsToString(filters)
   const json = await fetchURL(url + queryParams, { headers: { Authorization: 'Bearer ' + accessToken } })
-  return [json.message, json.playlists?.items]
+  const filteredPlaylists = filterPlaylists(json.playlists?.items, filters.query)
+
+  return [json.message, filteredPlaylists]
+}
+
+function filterPlaylists(playlists: Playlist[], query: string): Playlist[] {
+  return (playlists || []).filter((playlist: Playlist) => {
+    const lowerCaseName = playlist.name.toLowerCase()
+    const lowerCaseQuery = query.toLowerCase()
+    return lowerCaseName.includes(lowerCaseQuery)
+  })
 }
