83788b086fdea005efda8057e422d2252937cbf9
douglas <douglassantoselias@gmail.com>
2020-08-13 13:05:18 -0300

    Add login option when the token is expired

--- components/InvalidTokenAlert.tsx
+++ components/InvalidTokenAlert.tsx
@@ -1,9 +1,24 @@
 import { Alert } from 'antd'
 
+import { LoginSpotify } from './LoginSpotify'
+
 type Props = {
   message: string
 }
 
 export function InvalidTokenAlert({ message }: Props) {
-  return <Alert showIcon type="error" message="Erro" description={message} />
+  return (
+    <Alert
+      showIcon
+      type="error"
+      message="Erro"
+      description={
+        <>
+          {message}
+          <br />
+          <LoginSpotify />
+        </>
+      }
+    />
+  )
 }
