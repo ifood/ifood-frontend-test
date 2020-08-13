25a4ef228da988d565fb2cb75ef4013fb35c8f85
douglas <douglassantoselias@gmail.com>
2020-08-13 14:06:50 -0300

    Add filter to playlists request function

--- data/playlists.ts
+++ data/playlists.ts
@@ -1,9 +1,11 @@
 import { fetchURL } from './fetchURL'
+import { convertObjectParamsToString } from '../utils/convertParams'
 
 const url = 'https://api.spotify.com/v1/browse/featured-playlists?'
 
-export async function fetchFeaturedPlaylists(accessToken: string): Promise<Playlist[]> {
-  const json = await fetchURL(url, { headers: { Authorization: 'Bearer ' + accessToken } })
+export async function fetchFeaturedPlaylists(accessToken: string, filters: any): Promise<Playlist[]> {
+  const queryParams = convertObjectParamsToString(filters)
+  const json = await fetchURL(url + queryParams, { headers: { Authorization: 'Bearer ' + accessToken } })
   return json.playlists?.items
 }
 
