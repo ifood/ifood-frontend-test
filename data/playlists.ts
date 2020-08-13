a1a990614ab826fb5916a103394a74b90fae02db
douglas <douglassantoselias@gmail.com>
2020-08-13 19:08:18 -0300

    Add function to remove query from filter

--- data/playlists.ts
+++ data/playlists.ts
@@ -4,17 +4,29 @@ import { convertObjectParamsToString } from '../utils/queryParams'
 const url = 'https://api.spotify.com/v1/browse/featured-playlists?'
 
 export async function fetchFeaturedPlaylists(accessToken: string, filters: FilterWithTimestamp): Promise<[string, Playlist[]]> {
-  const queryParams = convertObjectParamsToString(filters)
+  const queryParams = convertObjectParamsToString(removeQueryProperty(filters))
   const json = await fetchURL(url + queryParams, { headers: { Authorization: 'Bearer ' + accessToken } })
   const filteredPlaylists = filterPlaylists(json.playlists?.items, filters.query)
 
   return [json.message, filteredPlaylists]
 }
 
+function removeQueryProperty(filters: FilterWithTimestamp) {
+  const filterWithoutQuery = {}
+
+  for (const key in filters) {
+    if (key !== 'query') {
+      filterWithoutQuery[key] = filters[key]
+    }
+  }
+
+  return filterWithoutQuery
+}
+
 function filterPlaylists(playlists: Playlist[], query: string): Playlist[] {
   return (playlists || []).filter((playlist: Playlist) => {
     const lowerCaseName = playlist.name.toLowerCase()
-    const lowerCaseQuery = query.toLowerCase()
+    const lowerCaseQuery = (query || '').toLowerCase()
     return lowerCaseName.includes(lowerCaseQuery)
   })
 }
