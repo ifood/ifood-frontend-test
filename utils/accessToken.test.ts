43f79706b02e5dfa74e45abd1f31f7b0594fd20d
douglas <douglassantoselias@gmail.com>
2020-08-13 22:25:46 -0300

    Fix wrong import filename

--- utils/accessToken.test.ts
+++ utils/accessToken.test.ts
@@ -1,4 +1,4 @@
-import { extractAccessToken } from './extractAccessToken'
+import { extractAccessToken } from './accessToken'
 
 test('Extract correct token', () => {
   const urlHash = '#access_token=BQBM54BUxxFedCXFwIq4ufj6w6O268zczvIJWCe6ErjeYNN_MU0BSAOafMcmGnMDYgnOuSF_sFSNr_PnaA2qBXOlPYPcOCtPIrDvlyGS4EK-FfRfx3LZUfDYazjolmI8vzCyY1lE9B6ZGMilQ2shIQ&token_type=Bearer&expires_in=3600'
