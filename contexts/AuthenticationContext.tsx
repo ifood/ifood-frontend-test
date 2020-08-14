f3f7df7f2f90d6dbde8b8a27f63af175559e82a0
Augusto Cheme <augusto.cheme@gmail.com>
2020-08-14 18:07:32 -0300

    (Refactor) Change useEffect dependency

--- contexts/AuthenticationContext.tsx
+++ contexts/AuthenticationContext.tsx
@@ -13,13 +13,14 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const router = useRouter();
 
+  const path = router.asPath;
+
   useEffect(() => {
-    const path = router.asPath;
     const params = qs.parse(path);
     const authToken = params['/playlists#access_token'];
     setToken(authToken);
     Cookie.set('USER_TOKEN', authToken);
-  }, [router]);
+  }, [path]);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated: !!token }}>
