9cbd4b7d0b122ec4859679d262865f79cf2ca98b
douglas <douglassantoselias@gmail.com>
2020-08-13 22:26:19 -0300

    Add env verification to redirect to correct url

--- components/LoginSpotify.tsx
+++ components/LoginSpotify.tsx
@@ -1,6 +1,8 @@
 const baseAuthorizeURL = 'https://accounts.spotify.com/authorize'
 const clientId = '3b0fa2d640414630b38e2198a9060422'
-const redirectURI = 'http://localhost:3000/'
+const devURL = 'http://localhost:3000/'
+const prodURL = 'https://spotifood.douglasselias.vercel.app/'
+const redirectURI = process.env.NODE_ENV === 'development' ? devURL : prodURL
 const authorizeURL = `${baseAuthorizeURL}?client_id=${clientId}&response_type=token&redirect_uri=${redirectURI}`
 
 export function LoginSpotify() {
