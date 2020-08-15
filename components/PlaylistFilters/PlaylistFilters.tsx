698144e33ad5e339c425a635c53527c9693fb81d
Augusto Cheme <augusto.cheme@gmail.com>
2020-08-15 16:12:09 -0300

    (Feature) Create Filters components

---
+++ components/PlaylistFilters/PlaylistFilters.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import usePlaylist from '../../contexts/PlaylistContext';
+import Filter from '../Filter/Filter';
+import Input from '../Input/Input';
+import { Container, Toggler } from './styles';
+
+const PlaylistFilters: React.FC = () => {
+  const { filterFields, filterByText } = usePlaylist();
+  const [showFilters, setShowFilters] = useState(false);
+
+  return (
+    <>
+      <Toggler onClick={() => setShowFilters(!showFilters)}>
+        {showFilters ? 'Fechar filtros' : 'Abrir filtros'}
+      </Toggler>
+      <Container
+        id="fields_container"
+        style={{
+          maxHeight: showFilters
+            ? document?.getElementById('fields_container').scrollHeight
+            : 0,
+        }}
+      >
+        <Input
+          id="name"
+          name="filter_name"
+          label="Nome"
+          onChange={filterByText}
+        />
+        {filterFields.map(filter => (
+          <Filter key={filter.id} filter={filter} />
+        ))}
+      </Container>
+    </>
+  );
+};
+
+export default PlaylistFilters;
---
+++ components/PlaylistFilters/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Toggler = styled.button`
+  background: #ff6363;
+  color: #fff;
+  border: none;
+  border-radius: 0.5rem;
+  padding: 10px;
+  font-weight: bold;
+  margin: 1rem 0;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  overflow: hidden;
+  transition: all ease 0.2s;
+`;
