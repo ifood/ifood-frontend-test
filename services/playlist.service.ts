dafe570015bb5294df81ec1cb0ff72c5c595e3ed
Augusto Cheme <augusto.cheme@gmail.com>
2020-08-15 16:18:02 -0300

    (Feature) Change playlist service

--- services/playlist.service.ts
+++ services/playlist.service.ts
@@ -1,5 +1,5 @@
 import useSWR from 'swr';
-import { spotifyApi } from './api';
+import { spotifyApi, mockyApi } from './api';
 import { PlaylistInterface } from '../interfaces/playlist.interface';
 
 export interface PlaylistResponse {
@@ -8,7 +8,7 @@ export interface PlaylistResponse {
 }
 
 type fetcherParams = {
-  limit: number;
+  limit?: number;
 };
 
 type Service = {
@@ -16,37 +16,8 @@ type Service = {
   fetcher: (params: fetcherParams) => Promise<PlaylistResponse>;
 };
 
-export function useService(service?: Service) {
-  return useSWR(service?.key, service?.fetcher, {
-    refreshInterval: 30000,
-  });
-}
-
-async function fetcher(
-  url: string,
-  params?: fetcherParams,
-): Promise<PlaylistResponse> {
-  try {
-    const response = await spotifyApi(url, {
-      method: 'GET',
-      params,
-    });
-
-    if (response.data.status >= 400 && response.data.status < 600) {
-      throw new Error(response.data.message);
-    }
-
-    return response.data;
-  } catch (e) {
-    throw new Error(e.response?.data?.message ?? e.message);
-  }
-}
-
-export const playlistService = {
-  featuredPlaylists(params: fetcherParams): Service {
-    return {
-      key: '/browse/featured-playlists',
-      fetcher: () => fetcher('/browse/featured-playlists', params),
-    };
-  },
+export const getFeaturedPlaylists = (params: fetcherParams) => {
+  return spotifyApi.get('/browse/featured-playlists', { params });
 };
+
+export const getChoicesForFilter = () => mockyApi.get('/');
