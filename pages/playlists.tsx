5d03f810ee8808f72b31abfef4c5999ddadc61a3
Augusto Cheme <augusto.cheme@gmail.com>
2020-08-15 16:16:42 -0300

    (Feature) Improve playlists page

--- pages/playlists.tsx
+++ pages/playlists.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
-import Cookie from 'js-cookie';
-import { useService, playlistService } from '../services/playlist.service';
 import PlaylistShowcase from '../components/PlaylistShowcase/PlaylistShowcase';
-import useAuth from '../contexts/AuthenticationContext';
+import { withContext } from '../hocs/withContext';
+import usePlaylists, { PlaylistProvider } from '../contexts/PlaylistContext';
 
 const PlaylistsPage: React.FC = () => {
-  const auth = useAuth();
-  const params = {
-    limit: 12,
-  };
-
-  const { data, error } = useService(
-    auth.isAuthenticated ? playlistService.featuredPlaylists(params) : null,
-  );
-
-  if (error) {
-    return <p>error</p>;
-  }
-
-  if (data == null) {
-    return <p>loading...</p>;
-  }
-
-  const { playlists, message } = data;
+  const context = usePlaylists();
+  const { playlists, featuredPlaylists } = context;
 
   return (
     <>
-      <PlaylistShowcase playlists={playlists.items} message={message} />
+      <PlaylistShowcase
+        playlists={playlists}
+        message={featuredPlaylists?.message}
+      />
     </>
   );
 };
 
-export default PlaylistsPage;
+export default withContext(PlaylistProvider, PlaylistsPage);
