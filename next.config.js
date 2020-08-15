bf73fc2517b6acb956094c6220ed530d56706fe9
Augusto Cheme <augusto.cheme@gmail.com>
2020-08-15 16:41:51 -0300

    (Feature) Change next.config for vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-module.exports = {
-  env: {
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+
+  console.log(`isDev:${isDev}  isProd:${isProd}`)
+
+  const env = {
     CLIENT_ID: process.env.CLIENT_ID,
-    REDIRECT_URI: process.env.REDIRECT_URI
-  },
-};
+    REDIRECT_URI: (() => {
+      if (isDev) return 'http://localhost:3000/playlists'
+      if (isProd) return 'https://spotifood.vercel.app/playlists'
+      return 'CALLBACK_URL:not (isDev,isProd && !isStaging,isProd && isStaging)'
+    })()
+  }
+
+  return {
+    env,
+  }
+}
