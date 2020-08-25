0b4356115ffccd6c630fa549d6eafed86518e1e6
LuAnderson <lucasand96@gmail.com>
2020-08-25 12:38:06 -0300

    feat(chore): create aplication with react, styledcomponent and gatsby

---
+++ src/components/hello/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+function Hello() {
+    return (
+        <S.Text>Hello iFood</S.Text>
+    );
+}
+
+export default Hello
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Hello } from '../components';
+
+function IndexPage() {
+  return (
+    <Hello />
+  )
+}
+
+export default IndexPage;
