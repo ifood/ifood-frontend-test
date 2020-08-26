12f65abeaa4b0cac0ad88fc6ce1ec7b9fc51f0e4
LuAnderson <lucasand96@gmail.com>
2020-08-26 09:37:29 -0300

    feat(playlist.service): remove sensitive token information from service

--- src/services/playlist/index.js
+++ src/services/playlist/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const headers = {
-    'Authorization': 'Bearer BQDeSxIrB8kPKfC_Tyg5Mqe_ZeRsVKyyPwYROKubJhKkNo0TxNL1ej5wBkn8nf6LUfH-bAngIxSfKYvZzK9OQ2XPNJZJp2t2FjQytXkm2Wrkjdh6w6Rpj_dYUMEDb0SJgmQXylqTa7X1EBub4lVnk0FrX9nb6B4hCTTsYg7hma0iPbWB08Dt9UQAixwgREt0b9MuNXSIhkA'
+    'Authorization': 'Bearer @ToDo: Insert OAuth Token'
 };
 
 const getPlaylist = async () => (
