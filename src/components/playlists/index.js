a513f9cb9036d033c178bb65adfcef1053fc3249
LuAnderson <lucasand96@gmail.com>
2020-08-26 09:24:49 -0300

    feat(playlist.c): add component Card to render information

--- src/components/playlists/index.js
+++ src/components/playlists/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Card from './card';
+
 import * as S from './styles';
 
 function Playlists({ list, title }) {
     return (
-        <>
+        <S.Container>
             <S.Text>{title}</S.Text>
-            {list.map((item, index) => (
-                <S.Title key={index}>{ item.name }</S.Title>
+            {list.map((item) => (
+                <Card 
+                    key={item.id}
+                    title={item.name}
+                    description={item.description}
+                    href={item.external_urls.spotify}
+                />
             ))}
-        </>
+        </S.Container>
     );
 }
 
--- src/components/playlists/styles.js
+++ src/components/playlists/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: block;
+`;
+
 export const Text = styled.h2`
   font-size: 14px;
 `;
