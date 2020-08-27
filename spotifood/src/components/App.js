cef12879e6a918cb0d1a88db8c537407c578d4c6
eli_rlima <elivelton@salvus.me>
2020-08-26 21:42:54 -0300

    [develop] | refact App component to rfc

--- spotifood/src/components/App.js
+++ spotifood/src/components/App.js
@@ -1,5 +1,5 @@
 // Global
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns'
 // Components
 import List from './List';
@@ -9,59 +9,54 @@ import { getPlaylists } from '../api';
 // Stylesheets
 import './App.scss';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [],
-            visible: false,
-            filters: {
-                locale: 'pt_BR',
-                country: 'BR',
-                offset: 0,
-                limit: 20,
-                timestamp: format(new Date(), 'yyyy-MM-dd') + 'T' + format(new Date(), 'HH:mm:ss'),
-            }
-        }
-    }
+function App() {
+    const [filters, setFilters] = useState({
+        locale: 'pt_BR',
+        country: 'BR',
+        offset: 0,
+        limit: 20,
+        timestamp: format(new Date(), 'yyyy-MM-dd') + 'T' + format(new Date(), 'HH:mm:ss'),
+    });
+    const [list, setList] = useState([]);
+    const [visible, setVisible] = useState(false);
 
-    onChangeFilter = field => async (e, target) => {
-        console.log(field);
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getPlaylists(filters);
+            const data = response.playlists.items;
+
+            setList(data);
+            setVisible(true);
+        }
+
+        fetchData();
+    }, [filters]);
+
+    const onChangeFilter = field => async (e, target) => {
         const { value } = target || e.target;
-        let { filters } = this.state;
 
         if (field === 'limit') {
-            filters[field] = field === 'limit' && (value < 1 || value > 50) ? this.state.filters.limit : value;
+            filters[field] = field === 'limit' && (value < 1 || value > 50) ? filters.limit : value;
         } else if ('offset') {
-            filters[field] = field === 'offset' && value < 0 ? this.state.filters.offset : value;
+            filters[field] = field === 'offset' && value < 0 ? filters.offset : value;
         } else {
             filters[field] = value;
         }
 
         const response = await getPlaylists(filters);
         const data = response.playlists.items;
-        this.setState({ filters, list: data, visible: true });
-    }
 
-    async componentDidMount() {
-        const { filters } = this.state;
-        this.setState({ visible: false });
-        const response = await getPlaylists(filters);
-        const data = response.playlists.items;
-
-        this.setState({ list: data, visible: true })
+        setVisible(true);
+        setList(data);
+        setFilters(filters);
     }
  
-    render() {
-        const { list, visible, filters } = this.state;
-        console.log(filters);
-        return(
-            <div className='page'>
-                <Filters visible={visible} onChange={this.onChangeFilter} filters={filters} />
-                <List data={list} />
-            </div>
-        );
-    }
+    return(
+        <div className='page'>
+            <Filters visible={visible} onChange={onChangeFilter} filters={filters} />
+            <List data={list} />
+        </div>
+    );
 }
 
 export default App;
