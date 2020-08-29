843bdecfd2476527a210cc0a39357474573f4fa2
LuAnderson <lucasand96@gmail.com>
2020-08-29 03:22:25 -0300

    feat(input.c): create component of the input

---
+++ src/components/filter/input/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as S from './styles';
+
+function Input({ defaultValue, id, max, min, type }) {
+    return (
+        <S.Input 
+            defaultValue={defaultValue}
+            id={id}
+            max={max}
+            min={min}
+            type={type}
+        />
+    );
+}
+
+Input.propTypes = {
+    defaultValue: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    max: PropTypes.number,
+    min: PropTypes.number,
+    type: PropTypes.string.isRequired,
+};
+
+Input.defaultProps = {
+    min: '0',
+};
+
+export default Input;
