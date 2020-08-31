fd49a2cf25bda0c0fcaa37385d58e9e31f70d2b6
LuAnderson <lucasand96@gmail.com>
2020-08-31 15:39:07 -0300

    feat(service/playlist.response): create mock of playlist response

---
+++ src/services/utils/mocks/playlist.response.js
@@ -0,0 +1,342 @@
+const playlistResponseMocks = {
+    "href" : "https://api.spotify.com/v1/browse/featured-playlists?country=BR&timestamp=2020-08-31T18%3A29%3A08&offset=0&limit=20",
+    "items" : [ {
+      "collaborative" : false,
+      "description" : "Os hits históricos da década de ouro do rock nacional. Foto: Cazuza",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DWUjoOWKKJzAy"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWUjoOWKKJzAy",
+      "id" : "37i9dQZF1DWUjoOWKKJzAy",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f0000000335f41fa2e61b0e6099d8835f",
+        "width" : null
+      } ],
+      "name" : "BRock 80",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU3Mjk3MjcyOCwwMDAwMDA4YTAwMDAwMTZlM2M3YjBmNjUwMDAwMDE2ZTNiZGRmMzMw",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWUjoOWKKJzAy/tracks",
+        "total" : 60
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DWUjoOWKKJzAy"
+    }, {
+      "collaborative" : false,
+      "description" : "O melhor da fusão do Forró e Sertanejo, com uma pitada de Arrocha! Foto: Solange Almeida, Márcia Fellipe",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DXbkU0pC52xDz"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXbkU0pC52xDz",
+      "id" : "37i9dQZF1DXbkU0pC52xDz",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003be9872bbaf68a18dbc9b4015",
+        "width" : null
+      } ],
+      "name" : "Forronejo",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5NzM3NDEyMCwwMDAwMDA4MjAwMDAwMTczZWFlYWMwZTAwMDAwMDE3MzExZGNmMWU4",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXbkU0pC52xDz/tracks",
+        "total" : 70
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DXbkU0pC52xDz"
+    }, {
+      "collaborative" : false,
+      "description" : "As maravilhosas novidades da MPB. Foto: Gilsons, Mariana Volker.",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DWTWoEMrRMHZ8"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWTWoEMrRMHZ8",
+      "id" : "37i9dQZF1DWTWoEMrRMHZ8",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003a4a80f6c6c13510f1a51de6a",
+        "width" : null
+      } ],
+      "name" : "Divina MPB",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5ODU4MzcyMCwwMDAwMDE3MzAwMDAwMTc0MzMwM2M4ZWYwMDAwMDE3NDMyZGE0Nzg5",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWTWoEMrRMHZ8/tracks",
+        "total" : 90
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DWTWoEMrRMHZ8"
+    }, {
+      "collaborative" : false,
+      "description" : "Os lançamentos mais esperados do Pagode estão aqui! Foto: Sambô",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DXaxIuRWwYocy"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXaxIuRWwYocy",
+      "id" : "37i9dQZF1DXaxIuRWwYocy",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f0000000321f41202d484e0630eee62e1",
+        "width" : null
+      } ],
+      "name" : "Alma Pagodeira",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5ODU4MzcyMCwwMDAwMDA0YjAwMDAwMTc0MzMwM2M5NDAwMDAwMDE3NDMyY2IxNGQ5",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXaxIuRWwYocy/tracks",
+        "total" : 80
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DXaxIuRWwYocy"
+    }, {
+      "collaborative" : false,
+      "description" : "Os sucessos e as novidades do Trap nacional estão aqui! Foto: Papatinho, Dfideliz, BIN, Anitta.",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DWWr5uieiPUVM"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWWr5uieiPUVM",
+      "id" : "37i9dQZF1DWWr5uieiPUVM",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003ec2e724b109ad7199d26778f",
+        "width" : null
+      } ],
+      "name" : "Trapperz Brasil",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5ODU4Mzg0MCwwMDAwMDBlMTAwMDAwMTc0MzMwNTlkNDgwMDAwMDE3NDMxZTY1NjFi",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWWr5uieiPUVM/tracks",
+        "total" : 60
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DWWr5uieiPUVM"
+    }, {
+      "collaborative" : false,
+      "description" : "Bem-vindo à playlist onde tudo é permitido! Foto: Mc Bruna Alves",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DWW2VVq6VvPuS"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWW2VVq6VvPuS",
+      "id" : "37i9dQZF1DWW2VVq6VvPuS",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f000000035be43cf5b8edb33c2cf16834",
+        "width" : null
+      } ],
+      "name" : "Funk Sem Filtro",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5ODU4Mzc4MCwwMDAwMDA2MTAwMDAwMTc0MzMwNGIzM2MwMDAwMDE3MmVkYWQ0MjU5",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DWW2VVq6VvPuS/tracks",
+        "total" : 100
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DWW2VVq6VvPuS"
+    }, {
+      "collaborative" : false,
+      "description" : "Os hits mais estourados no Nordeste e no Norte do Brasil! Foto: MC Rogerinho",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DXcedEPj5nsZ0"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXcedEPj5nsZ0",
+      "id" : "37i9dQZF1DXcedEPj5nsZ0",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003b221c45f5b6a8d2e307bfaa3",
+        "width" : null
+      } ],
+      "name" : "Paredão Explode",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5ODg4MjU3NywwMDAwMDBkYTAwMDAwMTc0NDRkM2ZjNWQwMDAwMDE3NDBkYmVkOTdm",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXcedEPj5nsZ0/tracks",
+        "total" : 70
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DXcedEPj5nsZ0"
+    }, {
+      "collaborative" : false,
+      "description" : "As grandes cantoras que comandam o Sertanejo! Foto: Lauana Prado",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DXcC14V9ZdJ9I"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXcC14V9ZdJ9I",
+      "id" : "37i9dQZF1DXcC14V9ZdJ9I",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003deabc365f8306782478d66bd",
+        "width" : null
+      } ],
+      "name" : "Rainhas do Sertanejo",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5Nzk3ODkyMCwwMDAwMDA2YjAwMDAwMTc0MGVmNzQ1NTYwMDAwMDE3NDBkYWY4OTIy",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXcC14V9ZdJ9I/tracks",
+        "total" : 60
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DXcC14V9ZdJ9I"
+    }, {
+      "collaborative" : false,
+      "description" : "Aqui tem lugar pro Brega, Lambada, Eletromelody e muito mais! Foto: Joelma",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DX1UbZv49dlIp"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DX1UbZv49dlIp",
+      "id" : "37i9dQZF1DX1UbZv49dlIp",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f000000038f78128aa00c80c4bc891093",
+        "width" : null
+      } ],
+      "name" : "Aparelhagem Brega",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5NTU1OTcyMCwwMDAwMDAxYTAwMDAwMTczN2VjNTM0ZTMwMDAwMDE3MzdkMzI1ZmM3",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DX1UbZv49dlIp/tracks",
+        "total" : 50
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DX1UbZv49dlIp"
+    }, {
+      "collaborative" : false,
+      "description" : "Carimbó, Guitarrada, Lambada e outros ritmos e influências da rica cultura do Pará e norte do Brasil. Foto: Dona Onete",
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/playlist/37i9dQZF1DXbFmIQ7xHuBO"
+      },
+      "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXbFmIQ7xHuBO",
+      "id" : "37i9dQZF1DXbFmIQ7xHuBO",
+      "images" : [ {
+        "height" : null,
+        "url" : "https://i.scdn.co/image/ab67706f00000003b25bbd4d0d2edb3515507e47",
+        "width" : null
+      } ],
+      "name" : "Treme Carimbó",
+      "owner" : {
+        "display_name" : "Spotify",
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/user/spotify"
+        },
+        "href" : "https://api.spotify.com/v1/users/spotify",
+        "id" : "spotify",
+        "type" : "user",
+        "uri" : "spotify:user:spotify"
+      },
+      "primary_color" : null,
+      "public" : null,
+      "snapshot_id" : "MTU5MzE0MDUyMCwwMDAwMDAwYzAwMDAwMTcyZWU5MzI1NzYwMDAwMDE3MGFmNDQzZTQz",
+      "tracks" : {
+        "href" : "https://api.spotify.com/v1/playlists/37i9dQZF1DXbFmIQ7xHuBO/tracks",
+        "total" : 68
+      },
+      "type" : "playlist",
+      "uri" : "spotify:playlist:37i9dQZF1DXbFmIQ7xHuBO"
+    } ],
+    "limit" : 20,
+    "next" : null,
+    "offset" : 0,
+    "previous" : null,
+    "total" : 10
+};
+
+export default playlistResponseMocks;
+
