7b517e14280906fe010a552a4fa679bdc851ef23
LuAnderson <lucasand96@gmail.com>
2020-09-01 16:15:08 -0300

    styles(select.c): change unity of the select

--- src/components/filter/select/styles.js
+++ src/components/filter/select/styles.js
@@ -4,7 +4,7 @@ export const Select = styled.select`
     border-radius: 1.25rem;
     padding: 5px 0 5px 10px;
     outline: none;
-    width: 100px;
+    width: 100%;
 `;
 
 export const Option = styled.option`
