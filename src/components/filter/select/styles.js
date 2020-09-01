b28d63b4cecf7dc43c253d8698bf0ed57beced78
LuAnderson <lucasand96@gmail.com>
2020-09-01 18:07:32 -0300

    doc(readme.md): create readme of the project

--- src/components/filter/select/styles.js
+++ src/components/filter/select/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Select = styled.select`
     border-radius: 1.25rem;
+    cursor: pointer;
     padding: 5px 0 5px 10px;
     outline: none;
     width: 100%;
