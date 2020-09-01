60a5538bf1a51aea7e6691da44d5ca7ff5875295
willanysilva <willanylp@gmail.com>
2020-09-01 11:48:00 -0300

    remove header on request

--- src/services/filter.js
+++ src/services/filter.js
@@ -1,14 +1,8 @@
 import axios from "axios";
 
-const FILTER_API = "http://www.mocky.io/v2/5a25fade2e0000213aa90776";
+const FILTER_API = "https://www.mocky.io/v2/5a25fade2e0000213aa90776";
 
 export const getFilters = async () => {
-  const result = await axios.get(FILTER_API, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      Origin: "http://localhost:3000",
-      Referer: "http://localhost:3000/",
-    },
-  });
+  const result = await axios.get(FILTER_API);
   return result.data;
 };
