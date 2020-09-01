a6660676bdbfb0eb04a22066eaad71aadcac688d
LuAnderson <lucasand96@gmail.com>
2020-09-01 16:13:03 -0300

    styles(input.c): improve styles of the input

--- src/components/filter/input/styles.js
+++ src/components/filter/input/styles.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import { desktop, mobile } from '../../../services/utils/styles/media-queries';
 
 export const Input = styled.input`
-    border: 1px solid black;
+    border: 1px solid #000;
     border-radius: 20px;
+    cursor: pointer;
     outline: none;
     padding: 4px 0 4px 10px;
-    width: 190px;
+    
+    ${mobile} {
+        width: 96%;
+    }
+
+    ${desktop} {
+        width: 190px;
+    }
 `;
