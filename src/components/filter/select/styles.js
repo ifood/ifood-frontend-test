bc532d547ff4859f21185ddac933a56cc45ec725
LuAnderson <lucasand96@gmail.com>
2020-09-01 13:51:08 -0300

    feat(select.c): edit custom visual kit of the select component

--- src/components/filter/select/styles.js
+++ src/components/filter/select/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Select = styled.select`
+    border-radius: 1.25rem;
+    padding: 5px 0 5px 10px;
+    outline: none;
+    width: 100px;
 `;
 
 export const Option = styled.option`
