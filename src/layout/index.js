27916eb6603de0061e4bb1b8b9d6ab0293187102
LuAnderson <lucasand96@gmail.com>
2020-09-01 17:16:09 -0300

    feat(layout.s): implements background image

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from "react-helmet"
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #000;
+    background-image: url(https://wallpaperaccess.com/full/1295555.jpg);
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+  }
+`
+
 const Layout = ({ children }) => (
     <>
+        <GlobalStyle />
         <Helmet>
             <meta charSet="utf-8" />
             <title>Spotifood</title>
