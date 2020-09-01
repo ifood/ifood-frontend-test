f38a2459b71b87bea8dda9c881d54b687e881e6c
LuAnderson <lucasand96@gmail.com>
2020-09-01 13:50:52 -0300

    feat(input.c): edit custom visual kit of the input component

--- src/components/filter/input/styles.js
+++ src/components/filter/input/styles.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
+    border: 1px solid black;
+    border-radius: 20px;
+    outline: none;
+    padding: 4px 0 4px 10px;
+    width: 190px;
 `;
