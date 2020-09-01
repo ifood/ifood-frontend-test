2dd650b2c17101ebef9b70036fffb8f3a9777b3b
willanysilva <willanylp@gmail.com>
2020-09-01 11:36:17 -0300

    add header on request

--- src/services/filter.js
+++ src/services/filter.js
@@ -3,6 +3,10 @@ import axios from "axios";
 const FILTER_API = "http://www.mocky.io/v2/5a25fade2e0000213aa90776";
 
 export const getFilters = async () => {
-  const result = await axios.get(FILTER_API);
+  const result = await axios.get(FILTER_API, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
   return result.data;
 };
