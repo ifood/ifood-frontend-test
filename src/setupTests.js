ffc151b5608385885c4c90990988b224c769a9cf
Sezar Thiago Caldeira <sezarthiago@gmail.com>
2020-09-04 12:13:20 -0300

    styled-components snapshot improvement

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-styled-components';
 import chai from 'chai';
 
 configure({ adapter: new Adapter() });
