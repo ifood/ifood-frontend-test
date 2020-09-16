e7f5e54912fa2b1bfee95611d501e027939df363
Mário Santos <mariocsants@gmail.com>
2020-09-16 19:27:58 -0300

    logo images

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import LogoImage from './styles';
 
-import spotifoodImage from '../../assets/images/spotifood.svg';
+import spotifoodBlackImage from '../../assets/images/spotifood-black.svg';
+import spotifoodWhiteImage from '../../assets/images/spotifood-white.svg';
+import spotifoodRedImage from '../../assets/images/spotifood-red.svg';
 
 interface LogoProps {
   width: string;
+  color?: 'black' | 'white' | 'red';
 }
 
-const Logo: React.FC<LogoProps> = ({ width }) => (
-  <LogoImage width={width} src={spotifoodImage} alt="Spotifood logo" />
-);
+const Logo: React.FC<LogoProps> = ({ width, color }) => {
+  const getImage = () => {
+    switch (color) {
+      case 'black':
+        return spotifoodBlackImage;
+      case 'red':
+        return spotifoodRedImage;
+      default:
+        return spotifoodWhiteImage;
+    }
+  };
 
-export default Logo;
+  return (
+    <LogoImage width={width} src={getImage()} alt="Spotifood logo" />
+  );
+};
+
+export default memo(Logo);
