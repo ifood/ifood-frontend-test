c1f66eb92a29837b2560f249b94c7dc544c102b4
Mário Santos <mariocsants@gmail.com>
2020-09-17 23:50:47 -0300

    playlist filters service

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,8 @@ const config = {
   clientId: process.env.REACT_APP_CLIENT_ID,
   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
   spotifyAccountUrl: 'https://accounts.spotify.com',
+  spotifyApi: 'https://api.spotify.com/v1',
+  playlistFiltersApi: 'http://www.mocky.io/v2/5a25fade2e0000213aa90776',
 };
 
 export default config;
