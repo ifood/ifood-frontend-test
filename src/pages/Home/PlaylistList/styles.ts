2224ff2880c456797ebdad511cede86146352405
Mário Santos <mariocsants@gmail.com>
2020-09-19 10:51:35 -0300

    playlists reponsive

--- src/pages/Home/PlaylistList/styles.ts
+++ src/pages/Home/PlaylistList/styles.ts
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  padding: 16px;
   display: grid;
-  grid-gap: 24px;
-  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  grid-gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
 
-  padding: 24px;
+
+  @media(min-width: 960px) {
+    padding: 24px;
+    grid-gap: 24px;
+  }
 `;
 
 export default Container;
