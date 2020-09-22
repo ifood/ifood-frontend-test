ba7389688be416a52d6a70e95144b31f6f10f87e
Eduardo  Drozda <eduardodrozda@agenciacodigo.com>
2020-09-22 20:27:02 -0300

    Criado estrutura inicial da tela de home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { Background } from "./styles";
+import { Background, MusicImage, Playlist } from "./styles";
+import cellphone from '../../assets/img/svg/cellphone.svg';
 
 const HomePage: React.FC = () => {
 
   return (
     <Background>
-
+      <Playlist>
+        qwe
+      </Playlist>
+      <MusicImage
+        src={cellphone}
+      />
     </Background>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,6 +4,27 @@ export const Background = styled.main`
   width: 100vw;
   height: 100vh;
   background-color: ${ ({ theme }) => theme.bomJour };
+
+  display: flex;
+`;
+
+export const MusicImage = styled.img`
+  width: 30vw;
+  margin 0 auto;
+
+  @media screen and (max-width: 900px) {
+      display: none;
+  }
+`
+
+export const Playlist = styled.aside`
+  background-color: #fff;
+  width: 60vw;
+
+  @media screen and (max-width: 900px) {
+    margin: 0 auto;
+    width: 90vw;
+  }
 `;
 
 
