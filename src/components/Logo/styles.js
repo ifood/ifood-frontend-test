ca8fdaa26a140691f6bcc9bbbb9a3b4cb3cba794
Rafael Paes <rfaelpaes@gmail.com>
2020-09-23 16:11:36 -0300

    feat: develops Logo component

---
+++ src/components/Logo/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+const Link = styled.a``
+
+const Text = styled.h2`
+  margin: 0;
+  color: var(--white);
+  font-weight: bold;
+  letter-spacing: 4px;
+`
+
+export { Container, Link, Text }
