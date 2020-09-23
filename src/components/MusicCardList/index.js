5ce868d9e9560c87ac373d4b2259bcc077d2fed1
Rafael Paes <rfaelpaes@gmail.com>
2020-09-23 17:10:07 -0300

    feat: develops MusicCardList component

---
+++ src/components/MusicCardList/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import { useFeaturedMusic } from '~/contexts'
+
+import { Container, Row, Col } from '~/components/Bootstrap'
+import { FontAwesomeIcon, faPlayCircle } from '~/components/Fontawesome'
+
+import Loading from '~/components/Loading'
+
+import {
+  Card,
+  Wrapper,
+  Link,
+  Image,
+  Details,
+  Name,
+  Description,
+} from './styles'
+
+export default function MusicCardList() {
+  const { loading, filterPlaylistName, playlistSearchName } = useFeaturedMusic()
+
+  return (
+    <Container>
+      {!!loading ? (
+        <Loading />
+      ) : (
+        <Row>
+          <Col>
+            <Card>
+              {!!filterPlaylistName(playlistSearchName) &&
+                filterPlaylistName(playlistSearchName).map((item, key) => (
+                  <Wrapper key={key}>
+                    <Link
+                      href={item.external_urls.spotify}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.images.map((image, key) => (
+                        <Image key={key} src={image.url} />
+                      ))}
+
+                      <Details className="details-row">
+                        <FontAwesomeIcon icon={faPlayCircle} size="3x" />
+
+                        <Name>{item.name}</Name>
+
+                        <Description>{item.description}</Description>
+                      </Details>
+                    </Link>
+                  </Wrapper>
+                ))}
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </Container>
+  )
+}
