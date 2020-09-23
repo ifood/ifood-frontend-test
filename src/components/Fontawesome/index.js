d0aeaa94fcdf45081caff0e4e89a1207730e7378
Rafael Paes <rfaelpaes@gmail.com>
2020-09-23 16:29:02 -0300

    chore: adds file for exporting fontawesome icons

---
+++ src/components/Fontawesome/index.js
@@ -0,0 +1,11 @@
+export { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+export {
+  faSearch,
+  faAlignLeft,
+  faHamburger,
+  faTimesCircle,
+} from '@fortawesome/free-solid-svg-icons'
+export {
+  faPlayCircle,
+  faCalendarAlt,
+} from '@fortawesome/free-regular-svg-icons'
