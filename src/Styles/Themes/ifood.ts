c9fdb2775337c053fbee22fe2ba61be65176e63e
Gilda Menezes <menezes.s.gilda@gmail.com>
2020-09-24 16:33:36 -0300

    add global style and create pages folders

---
+++ src/Styles/Themes/ifood.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'Ifood',
+
+        colors: {
+            first: '#F5F3F4',
+            second: '#EA1D2C',
+            third: '#DA5D69',
+
+            white: '#000',
+            black: '#FFF',
+            gray: '#3e3e3e',
+
+            on: '#F5F3F4',
+            off: '#000',
+        },
+};
+
---
+++ src/Styles/Themes/spotify.ts
@@ -0,0 +1,16 @@
+export default {
+    title: 'Spotify',
+
+        colors: {
+            first: '#000',
+            second: '#1ED760',
+            third: '#15883E',
+
+            white: '#FFF',
+            black: '#000',
+            gray: '#BFBFBF',
+
+            on: '#000',
+            off: '#F5F3F4',
+        },
+};
