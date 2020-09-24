8e2d267f038e510159b94253e7f995af55eff93e
Adriel Cavalcanti <cavalcantiadriel@gmail.com>
2020-09-24 20:48:24 -0300

    rename error handler page

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Auth from './pages/Auth';
-import Error from './pages/Error';
+import NotFound from './pages/NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/auth" component={Auth} />
-      <Route path="*" component={Error} />
+      <Route path="*" component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
