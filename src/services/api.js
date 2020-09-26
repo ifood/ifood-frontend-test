4636090a933d046e3734b97a10d2671ddcadbe93
Antonio Mesquita <antonio@parafernalia.net.br>
2020-09-26 20:56:08 -0300

    Fix: infinite redirect

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-import { refreshTokenRequest } from 'states/modules/session'
+// import { refreshTokenRequest } from 'states/modules/session'
 
-import { store } from 'states/store'
-import { unauthorized } from 'constant'
+// import { store } from 'states/store'
+// import { unauthorized } from 'constant'
 
 const spotifyApi = axios.create({
   baseURL: process.env.REACT_APP_SPOTIFY_API_URL,
@@ -17,15 +17,14 @@ const refreshTokenApi = axios.create({
   baseURL: process.env.REACT_APP_SPOTIFY_AUTH_URL,
 })
 
-spotifyApi.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    if (unauthorized.includes(error.response.status)) {
-      store.dispatch(refreshTokenRequest()) // TODO: max requests, for case we have an invalid token
-      return error
-    }
-  }
-)
+// spotifyApi.interceptors.response.use(
+//   (res) => res,
+//   (error) => {
+//     if (unauthorized.includes(error.response.status)) {
+//       store.dispatch(refreshTokenRequest()) // TODO: max requests, for case we have an invalid token
+//     }
+//   }
+// )
 
 export { spotifyApi, filterApi, refreshTokenApi }
 
