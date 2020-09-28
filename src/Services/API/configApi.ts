a9fd5b1e2fb6f436daf7f9ea5528efe42f86d87d
Gilda Menezes <menezes.s.gilda@gmail.com>
2020-09-28 00:17:27 -0300

    config

--- src/Services/API/configApi.ts
+++ src/Services/API/configApi.ts
@@ -1,5 +1,5 @@
 export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const REDIRECT_URI = process.env.URL;
+export const REDIRECT_URI = window.location.protocol + '//' + window.location.hostname;
 export const SCOPES = ['user-read-email'];
 
