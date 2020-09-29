83f78e0d00df63a287284203c3c13d93a7212755
Renata Mitsue <rmkarato@gmail.com>
2020-09-29 11:27:01 -0300

    initial application setup

---
+++ spotifood-test/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./assets/styles/global.css";
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <p>Teste</p>
+      </div>
+    );
+  }
+}
+
+export default App;
