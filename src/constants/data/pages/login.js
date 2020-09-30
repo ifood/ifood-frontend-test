18e7175f0b67cf30c89f073637b69409ed28f797
Vinicius Ribeiro <hello@vncsrbro.com>
2020-09-30 00:09:10 -0300

    refactor: removed mocked texts from Login page

---
+++ src/constants/data/pages/login.js
@@ -0,0 +1,6 @@
+export const loginData = {
+  title: "Where music meets <b>iFood</b>",
+  description:
+    "Discover new songs from our featured playlists while your order is on the way",
+  ctaLabel: "Start now",
+};
