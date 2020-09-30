72e881ecbb412c46f03ef34f039469181221e72a
Vinicius Ribeiro <hello@vncsrbro.com>
2020-09-29 22:05:13 -0300

    feat: add PlaylistCard component

---
+++ src/components/presentational/PlaylistCard/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import * as S from "./styled";
+
+const PlaylistCard = ({ link, image, title, author, label }) => {
+  return (
+    <S.PlaylistCard href={link} target="_blank">
+      <S.PlaylistCardImage>
+        <img src={image} alt={title} />
+      </S.PlaylistCardImage>
+
+      <S.PlaylistCardContent>
+        <S.PlaylistCardInfo>
+          <S.PlaylistCardTitle>{title}</S.PlaylistCardTitle>
+          <S.PlaylistCardAuthor>
+            by <S.PlaylistCardAuthorDetail>{author}</S.PlaylistCardAuthorDetail>
+          </S.PlaylistCardAuthor>
+        </S.PlaylistCardInfo>
+
+        <S.PlaylistCardLink>{label}</S.PlaylistCardLink>
+      </S.PlaylistCardContent>
+    </S.PlaylistCard>
+  );
+};
+
+PlaylistCard.propTypes = {
+  link: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default PlaylistCard;
---
+++ src/components/presentational/PlaylistCard/styled.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+import * as C from "constants/styles/colors";
+import * as T from "constants/styles/typography";
+import { SPACINGS } from "constants/styles/spacings";
+
+export const PlaylistCardLink = styled.span`
+  color: ${C.PRIMARY_COLOR};
+  margin: ${SPACINGS.xl} 0 auto;
+  text-align: center;
+  font-weight: bold;
+  font-size: ${T.SIZES.sm};
+  padding: ${SPACINGS.sm} 0 ${SPACINGS.sm};
+  border-bottom: 5px solid currentColor;
+  display: block;
+  transition: 300ms ease all;
+`;
+
+export const PlaylistCard = styled.a`
+  width: 100%;
+  max-width: 300px;
+  display: block;
+  margin: 100px;
+  position: relative;
+  top: 0;
+  transition: 300ms ease all;
+
+  &:hover {
+    top: -10px;
+    box-shadow: 0 4px 40px rgba(0, 0, 0, 0.15);
+
+    ${PlaylistCardLink} {
+      background-color: ${C.PRIMARY_COLOR_LIGHT};
+      color: ${C.LIGHT};
+      border-color: ${C.PRIMARY_COLOR_LIGHT};
+    }
+  }
+`;
+
+export const PlaylistCardImage = styled.div`
+  img {
+    display: block;
+  }
+`;
+
+export const PlaylistCardContent = styled.div`
+  border-top: 1px solid ${C.GREY_LIGHTER};
+  border-right: 1px solid ${C.GREY_LIGHTER};
+  border-left: 1px solid ${C.GREY_LIGHTER};
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  overflow: hidden;
+`;
+
+export const PlaylistCardInfo = styled.div`
+  padding: ${SPACINGS.lg} ${SPACINGS.lg} 0;
+`;
+
+export const PlaylistCardAuthor = styled.p`
+  color: ${C.GREY};
+  ${T.BODY_TEXT}
+`;
+
+export const PlaylistCardAuthorDetail = styled.span`
+  font-weight: bold;
+`;
+
+export const PlaylistCardTitle = styled.h2`
+  color: ${C.GREY_DARK};
+  margin: 0 auto ${SPACINGS.xs};
+  ${T.TITLE_MEDIUM};
+`;
