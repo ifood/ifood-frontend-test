458192deb7805087ea0ad77060569559a5d316d5
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-01 21:34:18 -0300

    refactor: add type prop to field component

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 
 import * as S from "./styled";
 
-const Field = ({ placeholder, as, children }) => {
+const Field = ({ placeholder, as, children, type }) => {
   return (
-    <S.Field as={as} placeholder={placeholder}>
+    <S.Field as={as} placeholder={placeholder} type={type}>
       {children && children}
     </S.Field>
   );
@@ -15,12 +15,14 @@ Field.defaultProps = {
   placeholder: null,
   children: null,
   as: null,
+  type: "text",
 };
 
 Field.propTypes = {
   placeholder: PropTypes.string,
   children: PropTypes.node,
   as: PropTypes.string,
+  type: PropTypes.string,
 };
 
 export default Field;
