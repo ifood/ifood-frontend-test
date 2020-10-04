a7e4217577064c69f6575abedcfe88b240e81752
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 01:18:43 -0300

    feat: add dynamic filters

--- src/containers/Filters/index.js
+++ src/containers/Filters/index.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Field from "components/Field";
+import Loader from "components/Loader";
+
+import useEffectUpdate from "hooks/useEffectUpdate";
+
+import { updatePlaylists } from "store/modules/playlists/actions";
+
+import { getFilterField } from "helpers/getFilterField";
+
+import FiltersApiService from "services/filters";
+import PlaylistsApiService from "services/playlists";
 
 import * as S from "./styled";
 
 const Filters = () => {
+  const [fields, setFields] = useState([]);
+  const [params, setParams] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const token = useSelector((state) => state.authentication.token);
+
+  useEffect(() => {
+    const requestFilters = FiltersApiService();
+    const { getFilters } = requestFilters;
+
+    getFilters()
+      .then((response) => {
+        setFields(response);
+      })
+      .catch((error) => {
+        return error;
+      });
+  }, []);
+
+  const handleInputChange = (e, id) => {
+    const { value } = e.target;
+
+    if (value !== "") {
+      const filter = {
+        [id]: value,
+      };
+
+      setParams((prevState) => {
+        return { ...prevState, ...filter };
+      });
+    }
+  };
+
+  useEffectUpdate(() => {
+    if (token) {
+      const requestPlaylists = PlaylistsApiService();
+      const { getPlaylists } = requestPlaylists;
+
+      getPlaylists(token, params)
+        .then((data) => {
+          dispatch(updatePlaylists(data));
+        })
+        .catch(() => {});
+    }
+  }, [params]);
+
   return (
     <S.Filters>
       <S.FiltersTitle>Browse in our playlists</S.FiltersTitle>
 
       <S.FiltersBox>
-        <S.FiltersFull>
-          <Field placeholder="Search by name" />
-        </S.FiltersFull>
+        {fields.length <= 0 && <Loader />}
 
-        <S.FiltersHalf>
-          <Field as="select">
-            <option value="" defaultValue disabled>
-              Locale
-            </option>
-            <option>Option</option>
-            <option>Option</option>
-          </Field>
-        </S.FiltersHalf>
-
-        <S.FiltersHalf>
-          <Field as="select">
-            <option value="" defaultValue disabled>
-              Country
-            </option>
-            <option>Option</option>
-            <option>Option</option>
-          </Field>
-        </S.FiltersHalf>
-
-        <S.FiltersHalf>
-          <Field placeholder="Quantity" type="number" />
-        </S.FiltersHalf>
-
-        <S.FiltersHalf>
-          <Field placeholder="Page" type="number" />
-        </S.FiltersHalf>
-
-        <S.FiltersFull>
-          <Field placeholder="Texto" type="date" />
-        </S.FiltersFull>
+        {fields.map((item) => (
+          <S.FiltersItem key={item.id}>
+            {getFilterField(item, (e) => handleInputChange(e, item.id))}
+          </S.FiltersItem>
+        ))}
       </S.FiltersBox>
     </S.Filters>
   );
