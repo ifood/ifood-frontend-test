b6c03ca7e3e9dc3953c63d15221aeb88a7c1da13
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 01:19:00 -0300

    feat: add dynamic playlists

--- src/containers/PlaylistsList/index.js
+++ src/containers/PlaylistsList/index.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Wrapper from "components/Wrapper";
 import PlaylistCard from "components/PlaylistCard";
+import Wrapper from "components/Wrapper";
+import Loader from "components/Loader";
+
+import PlaylistsApiService from "services/playlists";
+
+import { loadPlaylists } from "store/modules/playlists/actions";
 
 import * as S from "./styled";
 
 const PlaylistsList = () => {
+  const [tokenInvalid, setTokenInvalid] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.authentication.token);
+  const playlistsList = useSelector((state) => state.playlists);
+
+  useEffect(() => {
+    if (token) {
+      const requestPlaylists = PlaylistsApiService();
+      const { getPlaylists } = requestPlaylists;
+
+      getPlaylists(token)
+        .then((data) => {
+          dispatch(loadPlaylists(data));
+        })
+        .catch(() => {
+          setTokenInvalid(true);
+        });
+    }
+  }, [dispatch, token]);
+
   return (
     <Wrapper>
       <S.PlaylistsList>
-        {Array.from(Array(10), (_, i) => (
-          <PlaylistCard
-            key={i}
-            link="/"
-            image="https://i.scdn.co/image/ab67706f00000003aba1f07094bd3e98cd0122de"
-            title="Today's hits"
-            author="spotify"
-            label="Listen on spotify"
-          />
-        ))}
+        {playlistsList.length === 0 && <Loader />}
+
+        {tokenInvalid ? (
+          `Token invalido ou expirado`
+        ) : (
+          <>
+            {playlistsList.map((item) => (
+              <PlaylistCard
+                key={item.id}
+                title={item.name}
+                image={item.images.length >= 0 && item.images[0].url}
+                author={item.owner.display_name}
+                label="Listen on spotify"
+                link={item.external_urls.spotify}
+              />
+            ))}
+          </>
+        )}
       </S.PlaylistsList>
     </Wrapper>
   );
