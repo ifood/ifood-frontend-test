9a2a77c3e52f09ccc04358ade10d5fc49f9d36b6
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 01:15:58 -0300

    chore: add redux provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "store";
+
 import * as serviceWorker from "./serviceWorker";
 
 import Routes from "./routes";
@@ -8,8 +11,10 @@ import { GlobalStyle } from "./constants/styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Routes />
+    <Provider store={store}>
+      <GlobalStyle />
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
