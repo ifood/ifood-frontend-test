b0e07ce4c793cf0d38ab07a850e313884388f5ab
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 01:12:56 -0300

    chore: add config constants

---
+++ src/constants/config/index.js
@@ -0,0 +1,11 @@
+export const FILTERS_API_URL =
+  "http://www.mocky.io/v2/5a25fade2e0000213aa90776";
+
+export const SPOTIFY_API_URL =
+  "https://api.spotify.com/v1/browse/featured-playlists";
+
+export const REDIRECT_URL = "http://localhost:3000/playlists";
+
+export const SPOTIFY_CLIENTE_ID = "221bc20ee6b24107b53d81f0bbd66c18";
+
+export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENTE_ID}&response_type=token&redirect_uri=${REDIRECT_URL}`;
