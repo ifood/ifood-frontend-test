ab86d73252d5225f8c9493523412a9ec7be27970
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 01:20:12 -0300

    refactor: add auth validation

--- src/pages/Playlists/index.js
+++ src/pages/Playlists/index.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Head from "components/Head";
 import Filters from "containers/Filters";
 import PlaylistsList from "containers/PlaylistsList";
 
-const Playlists = () => (
-  <>
-    <Head />
-    <Filters />
-    <PlaylistsList />
-  </>
-);
+import userToken from "helpers/userToken";
+import { setUserAuth } from "store/modules/auth/actions";
+
+const Playlists = () => {
+  const dispatch = useDispatch();
+  const authServices = userToken();
+  const hasToken = authServices.checkToken();
+  const token = authServices.getToken();
+
+  useEffect(() => {
+    if (hasToken) {
+      const userAuthenticationState = {
+        isAuth: true,
+        token,
+      };
+
+      dispatch(setUserAuth(userAuthenticationState));
+    }
+  }, [dispatch, hasToken, token]);
+
+  return (
+    <>
+      {hasToken ? (
+        <>
+          <Head />
+          <Filters />
+          <PlaylistsList />
+        </>
+      ) : (
+        <Redirect to="/" />
+      )}
+    </>
+  );
+};
 
 export default Playlists;
