d299a164dbd3b80f3c2e201d85459fb722ebd05f
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-05 02:38:25 -0300

    style: add styles for each variation

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,11 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import * as C from "constants/styles/colors";
 import { SPACINGS } from "constants/styles/spacings";
 
-export const Button = styled.button`
+const buttonPrimary = css`
   background-color: ${C.LIGHT};
   color: ${C.PRIMARY_COLOR};
+
+  &:hover {
+    background: ${C.PRIMARY_COLOR_LIGHT};
+    color: ${C.LIGHT};
+  }
+`;
+
+const buttonSecondary = css`
+  background-color: ${C.PRIMARY_COLOR_LIGHT};
+  color: ${C.LIGHT};
+
+  &:hover {
+    background: ${C.PRIMARY_COLOR_DARK};
+  }
+`;
+
+export const Button = styled.button`
   padding: ${SPACINGS.xs} ${SPACINGS.xl};
   border-radius: 5px;
   outline: none;
@@ -14,8 +31,6 @@ export const Button = styled.button`
   font-size: 18px;
   transition: 300ms ease all;
 
-  &:hover {
-    background: ${C.PRIMARY_COLOR_LIGHT};
-    color: ${C.LIGHT};
-  }
+  ${({ variation }) => variation === "primary" && buttonPrimary};
+  ${({ variation }) => variation === "secondary" && buttonSecondary};
 `;
