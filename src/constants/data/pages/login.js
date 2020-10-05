2c5870ff389de5d358b8d5d123f6a64c974506a4
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-05 01:52:30 -0300

    chore: update login page literals

--- src/constants/data/pages/login.js
+++ src/constants/data/pages/login.js
@@ -1,6 +1,6 @@
 export const loginData = {
-  title: "Where music meets <b>iFood</b>",
+  title: "Está com fome de novidades?",
   description:
-    "Discover new songs from our featured playlists while your order is on the way",
-  ctaLabel: "Start now",
+    "Descubra novas músicas em nossas playlists enquanto seu pediddo está a caminho.",
+  ctaLabel: "Faça login",
 };
