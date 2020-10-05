be024d4a695d6238a1a07450f9e998d8db9d9f26
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-05 02:37:57 -0300

    refactor: add variation prop

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,18 @@ import PropTypes from "prop-types";
 
 import * as S from "./styled";
 
-const Button = ({ onClick, label }) => {
-  return <S.Button onClick={onClick}>{label}</S.Button>;
+const Button = ({ onClick, label, variation }) => {
+  return (
+    <S.Button variation={variation} onClick={onClick}>
+      {label}
+    </S.Button>
+  );
 };
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
+  variation: PropTypes.oneOf(["primary", "secondary"]).isRequired,
 };
 
 export default Button;
