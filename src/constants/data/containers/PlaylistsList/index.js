61eccc6e38b1f3309d3e0ede92297583164a372c
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-04 22:34:28 -0300

    chore: add erros messages

---
+++ src/constants/data/containers/PlaylistsList/index.js
@@ -0,0 +1,10 @@
+export const playlistsListContainerData = {
+  cta_label: "Volte para o login",
+  errors: {
+    invalid_token:
+      "Seu token expirou ou é inválido, por favor refaça seu login.",
+    empty_return: "Nenhuma playlist encontrada, refaça sua pesquisa.",
+  },
+  playlist_link_label: "Escute no spotify",
+  playlist_author_prefix: "por",
+};
