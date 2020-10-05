52a6a7fa66eb3305777e2d27b5c6c2db8342d96e
Vinicius Ribeiro <hello@vncsrbro.com>
2020-10-05 18:06:55 -0300

    refactor: add env file

--- src/constants/config/index.js
+++ src/constants/config/index.js
@@ -4,8 +4,4 @@ export const FILTERS_API_URL =
 export const SPOTIFY_API_URL =
   "https://api.spotify.com/v1/browse/featured-playlists";
 
-export const REDIRECT_URL = "http://localhost:3000/playlists";
-
-export const SPOTIFY_CLIENTE_ID = "221bc20ee6b24107b53d81f0bbd66c18";
-
-export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENTE_ID}&response_type=token&redirect_uri=${REDIRECT_URL}`;
+export const SPOTIFY_LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENTE_ID}&response_type=token&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}`;
