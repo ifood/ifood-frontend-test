ab1dd689488c2b705593ca46fea4c93b770282df
kimuradev <leandro2ki@gmail.com>
2020-10-05 21:54:39 -0300

    feat: add styled-components and axios

--- DETAILS.md
+++ DETAILS.md
@@ -1,9 +1,9 @@
 # Pendencies
 
-- styled-components
+- styled-components \*
 - axe
 - jest
 - react-testing-library
-- axios
+- axios \*
 - eslint
 - prettier
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 1.5em;
+`;
 
 function App() {
-  return <div>spotifood</div>;
+  return (
+    <div>
+      <H1>spotifood</H1>
+    </div>
+  );
 }
 
 export default App;
