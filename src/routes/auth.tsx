aed150a71157684d4bbf3ee1920a145ce211865c
kimuradev <leandro2ki@gmail.com>
2020-10-07 11:49:25 -0300

    feat: add auth route

---
+++ src/routes/auth.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, RouteComponentProps, Redirect, RouteProps } from 'react-router-dom';
+
+const AuthRoute: React.FC<RouteProps> = ({ component: Component, ...rest }) => {
+  let url = window.location.href;
+
+  function hasAcessToken(url: any) {
+    const access_token = url.match(/\#(?:access_token)\=([\S\s]*?)\&/);
+
+    if (!access_token) {
+      return false;
+    }
+
+    localStorage.setItem('access_token', access_token[1]);
+    return true;
+  }
+
+  if (!Component) {
+    return null;
+  }
+  const isLoggedIn = hasAcessToken(url);
+
+  return (
+    <Route
+      {...rest}
+      render={(props: RouteComponentProps<{}>) => (isLoggedIn ? <Component {...props} /> : <Redirect to="/" />)}
+    />
+  );
+};
+export default AuthRoute;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from 'pages/home';
 import { Error } from 'pages/error';
 import { Playlist } from 'pages/playlist';
+import AuthRoute from './auth';
 
 const Routes: React.FC = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/playlist" component={Playlist} />
+      <AuthRoute component={Playlist} path="/playlist" />
       <Route component={Error} />
     </Switch>
   </Router>
