9b6183a41f89e973967d2fae4d7b306a9e3740e2
Anna Fernandes <anna.cbf@gmail.com>
2020-10-07 23:42:44 -0300

    Add função de atualizar a página de playlists a cada 30 segundos

--- spotifood/src/pages/PlaylistsPage/PlaylistsPage.js
+++ spotifood/src/pages/PlaylistsPage/PlaylistsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Header } from '../../components/Header/Header';
 import { Playlists } from '../../components/Playlists/Playlists';
@@ -6,6 +6,12 @@ import { Playlists } from '../../components/Playlists/Playlists';
 import { Container } from '@material-ui/core';
 
 const PlaylistsPage = () => {
+    useEffect(() => {
+        setInterval(() => {
+            window.location.reload();
+        }, 30000);
+    }, []);
+
     return (
         <>
             <Header />
