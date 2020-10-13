4be5dc606e8476ad158919caaa0224c76ecabebf
thaylla <thayllachristineo@gmail.com>
2020-10-13 15:27:40 -0300

    feat: create Container component

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+const Container = ({ children }) => (
+  <S.Container>
+    {children}
+  </S.Container>
+);
+
+export default Container;
---
+++ src/components/Container/styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 16px auto;
+  width: calc(100% - 64px);
+`;
+
+export { Container };
