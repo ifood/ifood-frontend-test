9d453e34d3e82e755295ff1a328807089e263e27
thaylla <thayllachristineo@gmail.com>
2020-10-13 20:16:03 -0300

    feat: render Playlist component when user input some data

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -5,14 +5,16 @@ import Container from './../../components/Container';
 import InputSearch from './../../components/InputSearch';
 import SelectFilter from './../../components/SelectFilter';
 import InputFilter from './../../components/InputFilter';
+import Playlist from './../../components/Playlist';
 
-import getToken from './../../services/token'
 import getPlaylists from './../../services/playlists';
 import getFilters from './../../services/filters';
 
 const Search = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [filters, setFilters] = useState([]);
+  const [selectedOpt, setSelectedOpt] = useState([]);
+  const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -30,15 +32,56 @@ const Search = () => {
 
   }, []);
 
-  const hasResult = filters.length !== 0 && !isLoading;
+  const handleChange = async () => {
+    const searchByName = await getPlaylists();
+    return setPlaylists(searchByName.playlists.items);
+  };
+
+  const hasFilterResult = filters.length !== 0 && !isLoading;
+  const hasPlaylistResult = playlists.length !== 0 && !isLoading;
 
   return (
     <Container>
-      <InputSearch />
+      {hasPlaylistResult
+        && <S.FeatureTitle>Feature Playlists</S.FeatureTitle>
+      }
+
+      <S.Group>
+        {hasPlaylistResult
+          && playlists.map((playlist, key) => {
+
+            const {
+              external_urls: {
+                spotify,
+              },
+              images,
+              name,
+              description,
+            } = playlist;
+
+            return (
+              <Playlist
+                key={key}
+                href={spotify}
+                src={images[0].url}
+                alt={name}
+                title={name}
+                description={description}
+              />
+            )
+          })
+        }
+      </S.Group>
+
+      {hasPlaylistResult && <S.Line />}
+
+      <InputSearch
+        onChange={handleChange}
+      />
       <S.Title>Filtros</S.Title>
       <S.Group>
-        {hasResult
-          && filters.map(filter => {
+        {hasFilterResult
+          && filters.map((filter, key) => {
             const {
               id,
               name,
@@ -47,12 +90,13 @@ const Search = () => {
             } = filter;
 
             return (
-              <S.SubGroup>
+              <S.SubGroup key={key}>
                 <S.Subtitle>{name}</S.Subtitle>
                 {values
                   ? <SelectFilter
                     id={id}
                     name={name}
+                    onChange={e => setSelectedOpt(e.target.value)}
                     optValues={values}
                   />
                   : <InputFilter
