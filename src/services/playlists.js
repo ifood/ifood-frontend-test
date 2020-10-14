28092fc4f27fb8559481c476b9831c665a55cad2
thaylla <thayllachristineo@gmail.com>
2020-10-14 15:50:25 -0300

    feat: implements filters in playlists services

--- src/services/playlists.js
+++ src/services/playlists.js
@@ -9,7 +9,23 @@ const options = (token) => ({
 
 const getPlaylists = async (params) => {
   const token = await getToken();
-  const response = await fetch(`https://api.spotify.com/v1/browse/featured-playlists?country=SE&limit=2`, options(token));
+  setInterval(() => token, 30000);
+
+  const {
+    country,
+    locale,
+    timestamp,
+    limit,
+    offset
+  } = params;
+
+  const hasCountryParams = country && `country=${country}`;
+  const hasLocaleParams = locale && `locale=${locale}`;
+  const hasTimestampParams = timestamp && `timestamp=${timestamp}`
+  const hasLimitParams = limit && `limit=${limit}`;
+  const hasOffsetParams = limit && `offest=${offset}`
+
+  const response = await fetch(`https://api.spotify.com/v1/browse/featured-playlists?${hasCountryParams}&${hasLocaleParams}&${hasTimestampParams}&${hasLimitParams}&${hasOffsetParams}`, options(token));
   const result = await response.json();
 
   return result;
