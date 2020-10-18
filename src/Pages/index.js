909cbeadb7ac0c4cc6a86ad4eea9ea377bc0d038
Lais Vitoria <laisvitoria0607@gmail.com>
2020-10-18 18:42:07 -0300

    feat(): criação do componente de playlists em destaque

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../Components/Header'
+import FeaturedPlaylist from '../Components/FeaturedPlaylists'
 
 export default function Index(){
     return(
         <div>
             <Header/>
+            <FeaturedPlaylist/>
         </div>
     )
 }
