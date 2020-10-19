ef8999aafdc611f96d7bcb86b44d66533ba31023
Lais Vitoria <laisvitoria0607@gmail.com>
2020-10-19 14:49:16 -0300

    feat(master): subir os estados e requisições do componente de playlists em destaque de nível para acesso as informações em outro componente

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,13 +1,22 @@
 import React, {useState, useEffect} from 'react'
+import Spotify from 'spotify-web-api-js'
 
 import filtersApi from '../Services/filters-api.js'
+import playlistsApi from '../Services/playlists-api'
+
 import Header from '../Components/Header'
 import FeaturedPlaylist from '../Components/FeaturedPlaylists'
 
+const spotifyWebApi = new Spotify()
+
 export default function Index(){
+    const params = getHashParams()
     const [filters, setFilters] = useState([])    
     const [search, setSearch] = useState('')
-
+    const [logged, setLogged] = useState(params.access_token ? true : false)
+    const [playlists, setPlaylists] = useState([])
+    
+    //busca dos filtros
     useEffect(() => {
         filtersApi.get('/')
         .then(response =>{
@@ -23,10 +32,62 @@ export default function Index(){
         setSearch(event.target.value)
     }
 
+    useEffect(() => {
+        
+    },[search])
+    
+    //função disponibilizada pelo Spotify para pegar os parâmetros
+    function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while ( e = r.exec(q)) {
+           hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+      }
+
+    useEffect(() => {
+        async function Teste(){
+            if(params.access_token){
+                localStorage.setItem("token", params.access_token)
+                await playlistsApi.get('/', {
+                    headers: {
+                        Authorization: `Bearer ${params.access_token}`
+                    }
+                })
+                .then((response) => {
+                    setPlaylists(response.data.playlists.items)
+                    console.log(response.data.playlists.items)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+            }
+        }
+        Teste()
+    },[params])
+
+    setInterval(() => {
+        let token = localStorage.getItem("token")
+        playlistsApi.get('/', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then((response) => {
+            setPlaylists(response.data.playlists.items)
+                console.log(response.data.playlists.items)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, 30000)
+
     return(
         <div>
             <Header filters={filters} search={search} handleSearchChange={handleSearchChange}/>
-            <FeaturedPlaylist/>
+            <FeaturedPlaylist playlists={playlists} logged={logged}/>
         </div>
     )
 }
