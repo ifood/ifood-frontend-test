7d2f2e62acc35f43f488fef892135cbacebe1ae8
leoni <leonimega@hotmail.com>
2020-11-08 21:42:37 -0300

    use new constants and axios instance on api calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
-// import axios from "axios";
+import axios from "./axios";
 import queryString from "query-string";
-import { CLIENT_ID, BASE_SPOTIFY_URL } from "../constants";
+import {
+  CLIENT_ID,
+  BASE_SPOTIFY_AUTH_URL,
+  BASE_SPOTIFY_API_URL,
+} from "../constants";
 
 export const authorize = () => {
   const queryParams = queryString.stringify({
@@ -11,6 +15,9 @@ export const authorize = () => {
       "user-read-email playlist-modify-public user-modify-playback-state user-read-recently-played user-read-private playlist-read-private playlist-read-collaborative user-follow-read user-library-read user-top-read",
   });
 
-  const requestURL = `${BASE_SPOTIFY_URL}/authorize?${queryParams}`;
+  const requestURL = `${BASE_SPOTIFY_AUTH_URL}/authorize?${queryParams}`;
   window.location.href = requestURL;
 };
+
+export const getPlaylists = () =>
+  axios.get(`${BASE_SPOTIFY_API_URL}/v1/browse/featured-playlists`);
