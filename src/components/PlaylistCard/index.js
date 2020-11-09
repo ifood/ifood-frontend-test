d22183c1e98bde5ca4a1ed76373f7893b4d12a61
leoni <leonimega@hotmail.com>
2020-11-08 23:48:00 -0300

    render specific icons when playlist is collaborative or private

--- src/components/PlaylistCard/index.js
+++ src/components/PlaylistCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillLockFill, BsFillPeopleFill } from "react-icons/bs";
 
 export default function PlaylistCard({ playlist }) {
   const { images, collaborative, public: isPublic } = playlist;
@@ -9,6 +10,12 @@ export default function PlaylistCard({ playlist }) {
       className="playlist-card"
       aria-label={`Playlist named ${playlist.name}`}
     >
+      {isPublic === false && (
+        <BsFillLockFill className="playlist-card__private-icon" />
+      )}
+      {collaborative && (
+        <BsFillPeopleFill className="playlist-card__collab-icon" />
+      )}
       <div className="playlist-card__dark-cover" />
       <img className="playlist-card__img" src={images[0].url}></img>
     </div>
