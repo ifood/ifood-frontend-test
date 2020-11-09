3b4380a67d61fb3743cf96256998e744fa26db0e
leoni <leonimega@hotmail.com>
2020-11-09 10:43:47 -0300

    test some components that will be used as filters soon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { createTheme, BaseProvider } from "baseui";
 import { Router, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import PrivateRoute from "components/PrivateRoute";
@@ -6,6 +9,27 @@ import PublicOnlyRoute from "components/PublicOnlyRoute";
 import Landing from "containers/Landing";
 import Playlists from "containers/Playlists";
 import CallbackHandler from "containers/CallbackHandler";
+import { DatePicker } from "baseui/datepicker";
+
+const primitives = {
+  accent: "#EA1D2C",
+};
+
+const overrides = {
+  colors: {
+    borderFocus: primitives.accent,
+    menuFillHover: primitives.accent,
+    // inputFill: "white",
+    inputFillActive: "white",
+    inputBorder: "white",
+    menuFontHighlighted: "white",
+    calendarHeaderBackground: primitives.accent,
+    calendarDayBackgroundSelectedHighlighted: primitives.accent,
+  },
+};
+const theme = createTheme(primitives, overrides);
+
+const engine = new Styletron();
 
 export const history = createBrowserHistory();
 
@@ -13,13 +37,17 @@ class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <div className="app">
-          <Switch>
-            <PublicOnlyRoute path="/callback" component={CallbackHandler} />
-            <PublicOnlyRoute path="/intro" component={Landing} />
-            <PrivateRoute path="/" component={Playlists} />
-          </Switch>
-        </div>
+        <StyletronProvider value={engine}>
+          <BaseProvider theme={theme}>
+            <div className="app">
+              <Switch>
+                <PublicOnlyRoute path="/callback" component={CallbackHandler} />
+                <PublicOnlyRoute path="/intro" component={Landing} />
+                <PrivateRoute path="/" component={Playlists} />
+              </Switch>
+            </div>
+          </BaseProvider>
+        </StyletronProvider>
       </Router>
     );
   }
--- src/containers/PlaylistFilters/index.js
+++ src/containers/PlaylistFilters/index.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { getPlaylists, getFilters } from "services/api";
+import { getFilters } from "services/api";
+import { Select } from "baseui/select";
+import { DatePicker } from "baseui/datepicker";
+import { Input } from "baseui/input";
 
 export default function PlaylistFilters() {
   const [filters, setFilters] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [value, setValue] = React.useState("");
+  const [value2, setValue2] = React.useState([new Date()]);
+  const [value3, setValue3] = React.useState([new Date()]);
 
   const loadFilters = async () => {
     try {
@@ -25,5 +31,37 @@ export default function PlaylistFilters() {
 
   const renderFilter = () => {};
 
-  return <div className="playlist-filters">Filters: {filters.length}</div>;
+  return (
+    <div className="playlist-filters">
+      <Select
+        options={[
+          { label: "AliceBlue", id: "#F0F8FF" },
+          { label: "AntiqueWhite", id: "#FAEBD7" },
+          { label: "Aqua", id: "#00FFFF" },
+          { label: "Aquamarine", id: "#7FFFD4" },
+          { label: "Azure", id: "#F0FFFF" },
+          { label: "Beige", id: "#F5F5DC" },
+        ]}
+        value={value}
+        placeholder="Select color"
+        onChange={(params) => setValue(params.value)}
+      />
+      <DatePicker
+        clearable
+        formatString="yyyy-MM-dd HH:mm:ss" // dinamic
+        timeSelectStart
+        value={value2}
+        onChange={({ date }) => setValue2(Array.isArray(date) ? date : [date])}
+      />
+      <Input
+        value={value3}
+        max={3}
+        min={1}
+        onChange={(e) => setValue3(e.target.value)}
+        placeholder="Controlled Input"
+        type="number"
+        clearOnEscape
+      />
+    </div>
+  );
 }
