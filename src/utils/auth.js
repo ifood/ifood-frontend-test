b24c5bcd985ec5cc0cc39613a2b23a4dfe6c55c8
leoni <leonimega@hotmail.com>
2020-11-08 21:41:28 -0300

    add two new useful functions to handle auth token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,21 @@
+import { getHashParams } from "utils/spotify";
+
+export const getAuthItems = () => ({
+  accessToken: sessionStorage.getItem("access_token"),
+  expiresIn: sessionStorage.getItem("expires_in"),
+  tokenType: sessionStorage.getItem("token_type"),
+});
+
 export const hasValidAccessToken = () => {
   const accessToken = sessionStorage.getItem("access_token");
   const expiresIn = sessionStorage.getItem("expires_in");
 
   return accessToken && expiresIn > 60;
+};
+
+export const saveAuthItems = () => {
+  const parsed = getHashParams();
+  sessionStorage.setItem("access_token", parsed.access_token);
+  sessionStorage.setItem("expires_in", parsed.expires_in);
+  sessionStorage.setItem("token_type", parsed.token_type);
 };
