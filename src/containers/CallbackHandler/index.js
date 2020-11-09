765abfe3283fa910bf2612199e47dc17392dda08
leoni <leonimega@hotmail.com>
2020-11-08 21:23:44 -0300

    Add component that handles spotify auth callback

---
+++ src/containers/CallbackHandler/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { getHashParams } from "utils/spotify";
+
+export default function CallbackHandler() {
+  const history = useHistory();
+  useEffect(() => {
+    const parsed = getHashParams();
+    sessionStorage.setItem("access_token", parsed.access_token);
+    sessionStorage.setItem("expires_in", parsed.expires_in);
+    sessionStorage.setItem("token_type", parsed.token_type);
+    history.push("/playlists");
+  }, []);
+  return <div></div>;
+}
