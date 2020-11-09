75aac5aeccee330aef26cfe91d9c80537fa6f6f0
leoni <leonimega@hotmail.com>
2020-11-08 21:41:43 -0300

    use saveAuthItems on Callbackhandler

--- src/containers/CallbackHandler/index.js
+++ src/containers/CallbackHandler/index.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { getHashParams } from "utils/spotify";
+import { saveAuthItems } from "utils/auth";
 
 export default function CallbackHandler() {
   const history = useHistory();
   useEffect(() => {
-    const parsed = getHashParams();
-    sessionStorage.setItem("access_token", parsed.access_token);
-    sessionStorage.setItem("expires_in", parsed.expires_in);
-    sessionStorage.setItem("token_type", parsed.token_type);
+    saveAuthItems();
     history.push("/");
   }, []);
-  return <div></div>;
+  return null;
 }
