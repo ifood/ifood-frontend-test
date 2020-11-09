6f712ed73a284dc0b5e90d0123ab2ba381b906e1
leoni <leonimega@hotmail.com>
2020-11-08 21:41:56 -0300

    add spotify api url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const CLIENT_ID = "d66e378fe9ca4e02b7a0cbcb30379ab7";
-export const BASE_SPOTIFY_URL = "https://accounts.spotify.com";
+export const BASE_SPOTIFY_AUTH_URL = "https://accounts.spotify.com";
+export const BASE_SPOTIFY_API_URL = "https://api.spotify.com";
