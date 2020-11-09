e65e777c594cd420fb14e6624704e389ad9cf6a9
leoni <leonimega@hotmail.com>
2020-11-09 13:57:22 -0300

    add todo to fix mistake

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,13 +10,13 @@ export const hasValidAccessToken = () => {
   const accessToken = sessionStorage.getItem("access_token");
   const expiresIn = sessionStorage.getItem("expires_in");
 
-  return accessToken && expiresIn > 60;
+  return accessToken && expiresIn > 60; // TODO: calc timestamp instead of storing raw seconds
 };
 
 export const saveAuthItems = () => {
   const parsed = getHashParams();
   sessionStorage.setItem("access_token", parsed.access_token);
-  sessionStorage.setItem("expires_in", parsed.expires_in);
+  sessionStorage.setItem("expires_in", parsed.expires_in); // TODO: calc timestamp instead of storing raw seconds
   sessionStorage.setItem("token_type", parsed.token_type);
 };
 
