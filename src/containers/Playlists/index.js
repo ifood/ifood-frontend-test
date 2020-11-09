3bf3a027542d0e38718b54a52a35457b064d4fef
leoni <leonimega@hotmail.com>
2020-11-08 22:31:13 -0300

    call playlists api

--- src/containers/Playlists/index.js
+++ src/containers/Playlists/index.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getPlaylists } from "services/api";
+import { ReactComponent as YourSvg } from "assets/logo.svg";
 
 export default function Playlists() {
-  return <div className="playlists">Playlists</div>;
+  const [playlists, setPlaylists] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const loadPlaylists = async () => {
+    try {
+      setLoading(true);
+      const { data } = await getPlaylists();
+      if (data.playlists && Array.isArray(data.playlists.items)) {
+        setPlaylists(data.playlists.items);
+      }
+    } catch (e) {
+      console.log(e);
+      // TODO: Show error message on screen
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadPlaylists();
+  }, []);
+
+  return (
+    <div className="playlists">
+      <YourSvg />
+      Playlists {playlists.length}
+    </div>
+  );
 }
