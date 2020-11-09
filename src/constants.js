e1ffa2e8035e9645419d7c1ae1501495465f585b
leoni <leonimega@hotmail.com>
2020-11-09 15:46:46 -0300

    move timer constants to dedicated file

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const CLIENT_ID = "d66e378fe9ca4e02b7a0cbcb30379ab7";
 export const BASE_SPOTIFY_AUTH_URL = "https://accounts.spotify.com";
 export const BASE_SPOTIFY_API_URL = "https://api.spotify.com";
+
+export const PLAYLISTS_REFRESH_TIME = 30 * 1000;
+export const FILTERS_TRIGGER_TIMEOUT = 700;
