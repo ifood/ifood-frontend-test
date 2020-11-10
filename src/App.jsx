0642963bae0629e8c028e6460638b59db7282dd5
hugsil <hugo.silva@matera.com>
2020-11-10 11:37:08 -0300

    feat: to set up prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Container from './styles/container';
+import Header from './components/Header';
+
+import { Container } from './styles/container/Container';
 import GlobalStyles from './styles/globalStyle';
 
 function App() {
   return (
     <Container background>
-      <GlobalStyles></GlobalStyles>
+      <GlobalStyles />
+      <Header />
     </Container>
   );
 }
