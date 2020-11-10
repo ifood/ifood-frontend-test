17c0309db9f3c12759926f899df479ebf416dff3
hugsil <hugo.silva@matera.com>
2020-11-10 11:15:02 -0300

    feat: to set up styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Container from './styles/container';
+import GlobalStyles from './styles/globalStyle';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Container background>
+      <GlobalStyles></GlobalStyles>
+    </Container>
+  );
 }
 
 export default App;
