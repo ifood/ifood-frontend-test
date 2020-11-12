87a80071bedacf489192e17a1ff1c19ea293984c
hugsil <hugo.silva@matera.com>
2020-11-12 11:15:15 -0300

    chore: include css file of bootstrap and react-datepicker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
