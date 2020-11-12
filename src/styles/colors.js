fd277118353044f7eab451d4de3b0c389937ec35
hugsil <hugo.silva@matera.com>
2020-11-12 11:16:42 -0300

    feat: create global style and colors file

---
+++ src/styles/colors.js
@@ -0,0 +1,6 @@
+export const black = '#222222';
+export const gray = 'rgb(236, 236, 236)';
+export const green = 'rgb(0, 225, 85)';
+export const lightBlack = '#333';
+export const lightGray = 'rgb(179, 179, 179)';
+export const white = '#ffffff';
