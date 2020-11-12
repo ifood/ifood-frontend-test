c631dbc88ff6cf5c5c3024019fdcc826ed7d0ab6
hugsil <hugo.silva@matera.com>
2020-11-12 11:20:33 -0300

    feat: configure router and provider on app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import Header from './components/Header';
+/* eslint-disable react/prefer-stateless-function */
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import { Container } from './styles/container/Container';
+import { Component } from 'react';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+import store from './store';
+
 import GlobalStyles from './styles/globalStyle';
 
-function App() {
-  return (
-    <Container background>
-      <GlobalStyles />
-      <Header />
-    </Container>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <GlobalStyles />
+        <Router>
+          <Switch>
+            <Redirect path="/auth" to="/home" />
+            <Route path="/" exact component={Login} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
